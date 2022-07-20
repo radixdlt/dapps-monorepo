c23a660c98b49129127083d3617004aae5915949
AbstractFruitFactory <alexmedkex@gmail.com>
2022-07-20 13:50:06 +0100

    Navbar (#1)
    Implement navbar with animation

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import { css } from "@styles"
+  import { tweened, type Tweened } from "svelte/motion"
+  import { cubicOut } from "svelte/easing"
+  import { onMount } from "svelte"
+
+  const lineWidth = 50
 
   const navbar = css({
     marginTop: 15,
@@ -11,11 +16,54 @@
     marginLeft: 30,
     marginRight: 30
   })
+
+  const line = css({
+    position: "absolute",
+    border: "1px solid black",
+    width: `${lineWidth}px`,
+    marginTop: "20px",
+    borderRadius: "5px"
+  })
+
+  let homeRef: any
+  let selectedRef: any
+  let linePosition: Tweened<number>
+
+  const getAlignedXPosition = (element: any) =>
+    element.getBoundingClientRect().x -
+    (lineWidth / 2 - element.getBoundingClientRect().width / 2)
+
+  onMount(() => {
+    linePosition = tweened(getAlignedXPosition(homeRef), {
+      duration: 500,
+      easing: cubicOut
+    })
+  })
+
+  const select = (e: any) => {
+    console.log("target", e.target.getBoundingClientRect())
+    selectedRef = e.target
+  }
+
+  let offset: number
+  $: {
+    if (linePosition) {
+      if (selectedRef) {
+        linePosition.set(getAlignedXPosition(selectedRef))
+      }
+      offset = $linePosition
+    }
+  }
 </script>
 
 <div class={navbar()}>
+  {#if offset}
+    <div class={line()} style:left={`${offset}px`} />
+  {/if}
+
   <center>
-    <a class={link()} href="/explorer">explorer</a>
-    <a class={link()} href="/staking">staking</a>
+    <a bind:this={homeRef} class={link()} on:click={select} href="/">home</a>
+    <a class={link()} on:click={select} href="/explorer">explorer</a>
+    <a class={link()} on:click={select} href="/staking">staking</a>
   </center>
 </div>
