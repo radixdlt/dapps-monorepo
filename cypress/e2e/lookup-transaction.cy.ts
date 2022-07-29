21edac4b9b8b2a12fe5ecdae034acad39e696833
AbstractFruitFactory <>
2022-07-29 14:14:24 +0100

    refactor test

--- cypress/e2e/lookup-transaction.cy.ts
+++ cypress/e2e/lookup-transaction.cy.ts
@@ -1,7 +1,5 @@
-import { MAINNET_URL } from '../../src/constants'
-
-describe('empty spec', () => {
-  it('passes', () => {
+describe('searching for transaction', () => {
+  it('shows the transaction', () => {
     cy.intercept(
       {
         method: 'GET',
@@ -10,7 +8,7 @@ describe('empty spec', () => {
       {
         fixture: `transaction.json`
       }
-    ).as('transactionStatus') // and assign an alias
+    )
 
     cy.visit('/explorer')
     cy.get('input').type('ef71a9d6c63444fce6abd2df8fab2755cfb51f6794e578f60d99337193811842')
