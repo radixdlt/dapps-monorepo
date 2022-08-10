5d3be10dcd664e0b56797e54d069720a7258cd3d
AbstractFruitFactory <alexmedkex@gmail.com>
2022-08-10 12:56:46 +0100

    Validator list (#5)
    Add validator list with filters

--- src/routes/staking/index.svelte
+++ src/routes/staking/index.svelte
@@ -1,45 +1,8 @@
-<script lang="ts" context="module">
-    import type { Load } from './__types'
-    import { Gateway } from 'radix-js'
-    import { MAINNET_URL } from '@constants'
-  
-    export const prerender = false
-  
-    export const load: Load = async () => {
-      const response = await Gateway.validators(MAINNET_URL)
-      return {
-        props: { validators: await response.json() }
-      }
-    }
-  </script>
-  
-  <script lang="ts">
-    import { box, css } from '@styles'
-    import type { Validators } from '@types'
-  
-    export let validators: Validators
-  
-    const frame = box({
-      css: {
-        display: 'flex',
-        flexDirection: 'column'
-      }
-    })
-  
-    const validatorBox = css({
-      display: 'grid',
-      grid: `
-        searchByName acceptingStake stakePrc ownerStake fee uptime searchByAddress .
-        
-      `
-    })
-  </script>
-  
-  <div class={frame}>
-    {#each validators.validators as validator}
-      <div class={validatorBox()}>
-        {validator.properties.name}
-      </div>
-    {/each}
-  </div>
-  
+<script lang="ts">
+  import ValidatorBox from "@components/validator-box/ValidatorBox.svelte"
+  import type { Validators } from "@types"
+
+  export let validators: Validators
+</script>
+
+<ValidatorBox {validators} />
--- src/types.ts
+++ src/types.ts
@@ -41,51 +41,63 @@ export type Transaction = {
   }
 }
 
-type Validator = {
-  validator_identifier: {
-    address: string
-  }
-  stake: TokenAmount
-  info: {
-    owner_stake: TokenAmount
-    uptime: {
-      epoch_range: {
-        from: number
-        to: number
-      }
-      uptime_percentage: number
-      proposals_missed: number
-      proposals_completed: number
-    }
-  }
-  properties: {
-    url: string
-    validator_fee_percentage: number
-    name: string
-    registered: boolean
-    owner_account_identifier: {
-      address: string
-    }
-    external_stake_accepted: boolean
-  }
-  latest_fork_readiness_signal: {
-    signalled_at: {
-      version: number
-      timestamp: string
-      epoch: number
-      round: number
-    }
-    fork_id: string
-    fork_name: string
-  }
+export type Validator = {
+  address: string,
+  name: string,
+  totalStake: number,
+  ownerStake: number,
+  uptimePercentage: number,
+  feePercentage: number,
+  stakePercentage: number,
+  ownerStakePercentage: number,
+  stakeAccepted: boolean
 }
 
-export type Validators = {
+export type ValidatorsAPIResponse = {
   ledger_state: {
     epoch: number
     round: number
     timestamp: string
     version: number
-  }
-  validators: Validator[]
+  },
+  validators: {
+    validator_identifier: {
+      address: string
+    }
+    stake: TokenAmount
+    info: {
+      owner_stake: TokenAmount
+      uptime: {
+        epoch_range: {
+          from: number
+          to: number
+        }
+        uptime_percentage: number
+        proposals_missed: number
+        proposals_completed: number
+      }
+    }
+    properties: {
+      url: string
+      validator_fee_percentage: number
+      name: string
+      registered: boolean
+      owner_account_identifier: {
+        address: string
+      }
+      external_stake_accepted: boolean
+    }
+    latest_fork_readiness_signal: {
+      signalled_at: {
+        version: number
+        timestamp: string
+        epoch: number
+        round: number
+      }
+      fork_id: string
+      fork_name: string
+    }
+  }[]
 }
+
+export type Validators = Validator[]
