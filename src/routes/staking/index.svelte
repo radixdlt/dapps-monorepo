758c97bb0bd6cab23b8f4b2b94827739bcbe9c07
AbstractFruitFactory <>
2022-08-10 14:00:04 +0100

    prettier

--- src/routes/staking/index.svelte
+++ src/routes/staking/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import ValidatorBox from "@components/validator-box/ValidatorBox.svelte"
-  import type { Validators } from "@types"
+  import ValidatorBox from '@components/validator-box/ValidatorBox.svelte'
+  import type { Validators } from '@types'
 
   export let validators: Validators
 </script>
