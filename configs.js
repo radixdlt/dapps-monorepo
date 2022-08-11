81674652c7f86cf5b438c1b780d2513ff2b16fa2
Martin Lichstam <martin.lichstam@gmail.com>
2022-08-11 10:30:58 +0200

    Make alias universal across configs

--- configs.js
+++ configs.js
@@ -4,5 +4,14 @@ export const configs = {
 	},
 	url: {
 		MAINNET_URL: 'https://mainnet-gateway.radixdlt.com'
+	},
+	alias: {
+		'@components': 'src/components',
+		'@styles': 'src/styles.ts',
+		'@types': '/src/types.ts',
+		'@sdk': '/src/mock-sdk.ts',
+		'@stores': '/src/stores.ts',
+		'@utils': '/src/utils/utils.ts',
+		'@configs': '/configs.js'
 	}
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { configs } from './configs'
+import path from 'path'
+
+const mappedAliases = Object.entries(configs.alias).reduce(
+  (obj, [key, value]) => ({
+    [key]: path.resolve(__dirname, value),
+    ...obj
+  }),
+  {}
+)
 
 export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST })],
+  resolve: {
+    alias: {
+      ...mappedAliases,
+      $app: '/node_modules/@sveltejs/kit/assets/app'
+    }
+  },
   test: {
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     globals: true,
     environment: 'jsdom'
   }
