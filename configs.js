ffba9383f698dbec726d31f06824957277746690
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-08-16 14:39:10 +0100

    remove url from config

--- configs.js
+++ configs.js
@@ -2,9 +2,6 @@ export const configs = {
 	flags: {
 		isVitebook: process.env.VITEBOOK === 'true'
 	},
-	url: {
-		MAINNET_URL: 'https://mainnet-gateway.radixdlt.com'
-	},
 	alias: {
 		'@components': 'src/components',
 		'@styles': 'src/styles.ts',
