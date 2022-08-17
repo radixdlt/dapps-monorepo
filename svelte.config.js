737177c15dcf4e2613392f1b707e773cc30d3882
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-08-17 08:38:58 +0100

    fix import alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
+import { configs } from './configs'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,16 +11,7 @@ const config = {
 		prerender: {
 			default: true
 		},
-		alias: {
-			'@components': 'src/components',
-			'@styles': 'src/styles.ts',
-			'@types': 'src/types.ts',
-			'@sdk': 'src/mock-sdk.ts',
-			'@stores': 'src/stores.ts',
-			'@utils': 'src/utils/utils.ts',
-			'@configs': 'configs.js',
-			'@constants': 'src/constants.ts'
-		}
+		alias: configs.alias
 	}
 }
 
