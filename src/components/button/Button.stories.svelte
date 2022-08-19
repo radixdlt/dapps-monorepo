581952223b397d27597e519a1fc0721047da18f1
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-08-19 10:11:10 +0100

    add storybook

---
+++ src/components/button/Button.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Button from './Button.svelte'
+</script>
+
+<Meta title="Button" component={Button} />
+
+<Template let:args>
+  <Button>Hello</Button>
+</Template>
+
+<Story name="Primary" />
---
+++ src/components/connect-button/ConnectButton.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import ConnectButton from './ConnectButton.svelte'
+</script>
+
+<Meta title="ConnectButton" component={ConnectButton} />
+
+<Template let:args>
+  <ConnectButton>Hello</ConnectButton>
+</Template>
+
+<Story name="Primary" />
