e8118773e646da18bd9b5b48170c3ff972dd07c5
Martin Lichstam <martin.lichstam@gmail.com>
2022-08-24 11:57:20 +0200

    Add box component

---
+++ src/components/box/Box.stories.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Box from './Box.svelte'
+</script>
+
+<Meta title="Box" component={Box} />
+
+<Template>
+  <Box background="primaryButton">Hello</Box>
+  <Box py="medium" my="medium" background="primaryButton">Hello</Box>
+</Template>
+
+<Story name="Primary" />
