8a9846f5c525fb167fcf87428ec279791f95ed06
Martin Lichstam <martin.lichstam@gmail.com>
2022-08-25 11:47:01 +0200

    Change header stylings and make it more responsive

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,43 +1,21 @@
 import { css } from '@styles'
 
-export const connectBtn = css({
-  gridItem: 'connect',
-  justifySelf: 'right',
-  alignSelf: 'center',
-  marginRight: '$3xl'
-})()
-
-export const logo = css({
-  gridItem: 'logo',
-  fontFamily: 'StreetFighter',
-  fontSize: '$4xl',
-  justifySelf: 'center',
-  alignSelf: 'center'
-})
-
 export const header = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  p: '$md',
+  flexWrap: 'wrap',
   variants: {
     layout: {
-      mobile: {
-        // TODO
-      },
       desktop: {
-        display: 'grid',
-        grid: '100% / 25% 50% 25%',
-        height: 80,
-        backgroundColor: '$success'
+        justifyContent: 'space-between'
       }
     }
   }
 })({
   layout: {
-    '@small': 'mobile',
     '@medium': 'desktop',
     '@large': 'desktop'
   }
 })
-
-export const navbar = css({
-  justifySelf: 'center',
-  alignSelf: 'center'
-})()
