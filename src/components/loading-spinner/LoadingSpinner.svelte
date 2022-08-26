db2744dc77460597cfa0c85b94904605b7443b66
Martin Lichstam <34944192+lichstam@users.noreply.github.com>
2022-08-26 10:25:04 +0200

    change color to primary on spinner (#33)

--- src/components/loading-spinner/LoadingSpinner.svelte
+++ src/components/loading-spinner/LoadingSpinner.svelte
@@ -1,44 +1,46 @@
-<div class="lds-ring">
+<script lang="ts">
+  import { css, keyframes } from '@styles'
+  const rotate = keyframes({
+    '0%': {
+      transform: 'rotate(0deg)'
+    },
+    '100%': {
+      transform: 'rotate(360deg)'
+    }
+  })
+
+  const spinnerColor = css({
+    display: 'inline-block',
+    position: 'relative',
+    width: '80px',
+    height: '80px',
+    div: {
+      boxSizing: 'border-box',
+      display: 'block',
+      position: 'absolute',
+      width: '64px',
+      height: '64px',
+      margin: '8px',
+      border: '8px solid #fff',
+      borderRadius: 50,
+      animation: `${rotate} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite`,
+      borderColor: '$primary transparent transparent transparent'
+    },
+    'div:nth-child(1)': {
+      animationDelay: '-0.45s'
+    },
+    'div:nth-child(2)': {
+      animationDelay: '-0.3s'
+    },
+    'div:nth-child(3)': {
+      animationDelay: '-0.15s'
+    }
+  })
+</script>
+
+<div class={spinnerColor()}>
   <div />
   <div />
   <div />
   <div />
 </div>
-
-<style>
-  .lds-ring {
-    display: inline-block;
-    position: relative;
-    width: 80px;
-    height: 80px;
-  }
-  .lds-ring div {
-    box-sizing: border-box;
-    display: block;
-    position: absolute;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border: 8px solid #fff;
-    border-radius: 50%;
-    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: #fff transparent transparent transparent;
-  }
-  .lds-ring div:nth-child(1) {
-    animation-delay: -0.45s;
-  }
-  .lds-ring div:nth-child(2) {
-    animation-delay: -0.3s;
-  }
-  .lds-ring div:nth-child(3) {
-    animation-delay: -0.15s;
-  }
-  @keyframes lds-ring {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-</style>
