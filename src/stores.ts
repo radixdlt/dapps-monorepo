bdda0ca71e8b6984402f0bfc358e98796f6fa2d2
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-08-26 11:49:47 +0100

    remove unused store

--- src/stores.ts
+++ src/stores.ts
@@ -4,5 +4,3 @@ import type { Account, UserInfo } from 'radix-js'
 export const accounts = writable<UserInfo['accounts']>(undefined)
 
 export const selectedAccount = writable<Account>(undefined)
-
-export const currentPage = writable<'home' | 'staking' | 'explorer'>(undefined)
