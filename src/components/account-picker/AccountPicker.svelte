14a0d56b140516f24c8967c95efed4d0034f72fd
AbstractFruitFactory <alexmedkex@gmail.com>
2022-09-20 09:41:30 +0100

    Select (#53)
    * add base components folder
    
    * add select component
    
    * change size api

--- src/components/account-picker/AccountPicker.svelte
+++ src/components/account-picker/AccountPicker.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
+  import Select from '@components/_base/select/Select.svelte'
   import { accounts, selectedAccount } from '@stores'
   import { shortenAddress } from '@utils'
-  import { selectAcc } from './styles'
 </script>
 
-<select
-  class={selectAcc}
-  on:change={(e) => {
-    selectedAccount.set($accounts[e.currentTarget.selectedIndex])
-  }}
-  name="accounts"
-  style:visibility={$accounts ? 'visible' : 'hidden'}
+<Select
+  onChange={(e) =>
+    selectedAccount.set($accounts[e.currentTarget.selectedIndex])}
+  visible={$accounts !== undefined}
+  width="$3xl"
 >
   {#if $accounts}
     {#each $accounts as account}
@@ -20,4 +18,4 @@
       </option>
     {/each}
   {/if}
-</select>
+</Select>
