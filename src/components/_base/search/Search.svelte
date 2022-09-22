82198239104911f16d05a194b00d776a404e429e
Martin Lichstam <martin.lichstam@gmail.com>
2022-09-22 16:40:17 +0200

    Search component adjustments

--- src/components/_base/search/Search.svelte
+++ src/components/_base/search/Search.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
-  import { css, type SpaceKeys } from '@styles'
+  import { css, config } from '@styles'
 
   export let placeholder: string = ''
   export let value: string | undefined = undefined
-  export let width: string = ''
-  export let borderRadius: SpaceKeys = '$md'
 
-  $: style = css({
+  const style = css({
     color: '$onSurface',
     border: 'none',
-    borderRadius,
-    padding: '$sm',
-    background: "url('/images/search_icon.svg') no-repeat",
-    backgroundSize: '20px',
-    backgroundPosition: 'right 10px center',
-    backgroundColor: '$surface'
+    borderRadius: '$searchBorder',
+    py: '$md',
+    px: '$lg',
+    background: "url('/images/search_icon.svg') no-repeat", // TODO: use icon component / make it work nicely for dark mode
+    backgroundSize: config.theme.sizes.xs,
+    backgroundPosition: `right ${config.theme.space.lg} center`,
+    backgroundColor: '$surface',
+    shadow: true,
+    width: '$1'
   })()
 </script>
 
-<input style:width bind:value class={style} {placeholder} type="text" />
+<div>
+  <input bind:value class={style} {placeholder} type="text" />
+  <span />
+</div>
