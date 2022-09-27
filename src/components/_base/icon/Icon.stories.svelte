01e752c700dad1919ddf77c7006ce3f7597c6b5c
Martin Lichstam <martin.lichstam@gmail.com>
2022-09-27 16:26:24 +0200

    Add icon component

---
+++ src/components/_base/icon/Icon.stories.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Icon from './Icon.svelte'
+  import Box from '../box/Box.svelte'
+</script>
+
+<Meta title="Icon" component={Icon} />
+
+<Template>
+  Active state
+  <Box>
+    <Icon type="dashboard" interactive isActive />
+    <Icon type="blocks" isActive />
+    <Icon type="tokens" isActive />
+    <Icon type="transactions" isActive />
+    <Icon type="validators" isActive />
+    <Icon type="accounts" isActive />
+  </Box>
+  Inactive state
+  <Box>
+    <Icon type="dashboard" />
+    <Icon type="blocks" />
+    <Icon type="tokens" />
+    <Icon type="transactions" />
+    <Icon type="validators" />
+    <Icon type="accounts" />
+  </Box>
+</Template>
+
+<Story name="Primary" />
---
+++ src/components/_base/icon/Icon.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Image from '../image/Image.svelte'
+
+  const icons = {
+    dashboard: '/icons/dashboard.svg',
+    blocks: '/icons/blocks.svg',
+    tokens: '/icons/tokens.svg',
+    transactions: '/icons/transactions.svg',
+    accounts: '/icons/accounts.svg',
+    validators: '/icons/validators.svg'
+  }
+
+  export let isActive = false
+  export let type: keyof typeof icons
+  export let interactive: boolean | undefined = undefined
+
+  const style = {
+    width: '$sm',
+    height: '$sm',
+    cursor: interactive ? 'pointer' : 'default',
+    filter: !isActive ? 'grayscale(100%)' : '',
+    '&:active': {
+      filter: 'grayscale(100%)'
+    },
+    '&:hover': {
+      filter: '$iconHover'
+    }
+  }
+</script>
+
+<Image cx={style} width="$sm" height="$sm" alt="dashboard" src={icons[type]} />
