3db6055e4f5ed8f3b0623adc9b75fa69c29c1726
Martin Lichstam <martin.lichstam@gmail.com>
2022-10-12 17:40:50 +0200

    Add mutate to lib

---
+++ src/query-lib/mutations.ts
@@ -0,0 +1,15 @@
+import WalletSdk from '@radixdlt/wallet-sdk'
+import { makeQueries } from './_make-queries'
+
+export const requestAddresses = makeQueries({
+  fn: async () => {
+    const sdk = WalletSdk()
+    const res = await sdk.request({
+      accountAddresses: 'any'
+    })
+    if (res.isOk()) return res.value
+    else throw Error(res.error.message)
+  },
+  decoder: (res) => res,
+  transformationFn: (res) => res
+})
