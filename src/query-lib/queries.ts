d225b05796fc9ef037d785203a3c555056a52795
Martin Lichstam <martin.lichstam@gmail.com>
2022-10-14 11:43:34 +0200

    Move validator fetch to queries

--- src/query-lib/queries.ts
+++ src/query-lib/queries.ts
@@ -1,6 +1,11 @@
 import { RequestAddressesIO } from '@io/wallet'
 import WalletSdk from '@radixdlt/alphanet-walletextension-sdk'
 import { makeQueries } from './_make-queries'
+import { MAINNET_URL } from '@constants'
+import { Gateway } from 'radix-js'
+import { ValidatorArrayIO, ValidatorTransformedArrayIO } from '@io/gateway'
+import BigNumber from 'bignumber.js'
+import { toWholeUnits } from '@utils'
 
 export const requestAddresses = makeQueries({
   fn: async () => {
@@ -13,4 +18,36 @@ export const requestAddresses = makeQueries({
   },
   decoder: (res) => RequestAddressesIO.parse(res),
   transformationFn: (res) => res
+})
+
+export const getValidators = makeQueries({
+  fn: async () => Gateway.validators(MAINNET_URL),
+  decoder: ValidatorArrayIO.parse,
+  transformationFn: (res) => {
+    const totalStake = res.validators.reduce(
+      (accumulatedStake, validator) =>
+        accumulatedStake.plus(validator.stake.value),
+      BigNumber(0)
+    )
+    const transformedValidators = res.validators.map((validator) => ({
+      address: validator.validator_identifier.address,
+      name: validator.properties.name,
+      totalStake: toWholeUnits(validator.stake.value),
+      ownerStake: toWholeUnits(validator.info.owner_stake.value),
+      uptimePercentage: validator.info.uptime.uptime_percentage,
+      feePercentage: validator.properties.validator_fee_percentage,
+      stakeAccepted: validator.properties.external_stake_accepted,
+      stakePercentage: BigNumber(validator.stake.value)
+        .div(totalStake)
+        .multipliedBy(100)
+        .decimalPlaces(2)
+        .toNumber(),
+      ownerStakePercentage: BigNumber(validator.info.owner_stake.value)
+        .div(validator.stake.value)
+        .multipliedBy(100)
+        .decimalPlaces(2)
+        .toNumber()
+    }))
+    return ValidatorTransformedArrayIO.parse(transformedValidators)
+  }
 })
