cfa437a80d1000c3f2dc96285ae8351abf2a2a76
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-10-18 08:58:47 +0100

    wip added loading to validators

--- src/routes/staking/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { queryServer } from '@queries'
-import type { PageServerLoad } from './$types'
-
-export const prerender = true
-
-export const load: PageServerLoad = async () => ({
-  validators: await queryServer('getValidators')
-})
--- src/routes/staking/+page.svelte
+++ src/routes/staking/+page.svelte
@@ -4,13 +4,17 @@
 
 <script lang="ts">
   import ValidatorList from '@components/validator-list/ValidatorList.svelte'
-  import type { PageData } from './$types'
   import Card from '@components/_base/card/Card.svelte'
   import IconTextItem from '@components/icon-text-item/IconTextItem.svelte'
   import Search from '@components/_base/search/Search.svelte'
   import Box from '@components/_base/box/Box.svelte'
+  import { queryServer } from '@queries'
+  import type { ValidatorTransformedArray } from '@io/gateway'
+  import LoadingSpinner from '@components/_base/loading-spinner/LoadingSpinner.svelte'
 
-  export let data: PageData
+  let validators: ValidatorTransformedArray
+
+  queryServer('getValidators').then((res) => (validators = res))
 
   let filtered = ''
 </script>
@@ -19,10 +23,15 @@
   <Box m="large" transparent>
     <Search bind:value={filtered} />
   </Box>
-  <Card>
-    <IconTextItem bold isIconColor icon="transactions" slot="header"
-      >Staking</IconTextItem
-    >
-    <ValidatorList {filtered} slot="body" data={data.validators} />
-  </Card>
+  {#if validators}
+    <Card>
+      <IconTextItem bold isIconColor icon="transactions" slot="header"
+        >Staking</IconTextItem
+      >
+      
+      <ValidatorList {filtered} slot="body" data={validators} />
+    </Card>
+  {:else}
+    <LoadingSpinner />
+  {/if}
 </Box>
