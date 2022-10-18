a224362201449698a439fc80ae3857b1ca7bab6f
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-10-18 11:06:54 +0100

    add loading to deploy package

--- src/routes/deploy-package/+page.svelte
+++ src/routes/deploy-package/+page.svelte
@@ -1,9 +1,12 @@
 <script>
   import DeployPackage from '@components/deploy-package/DeployPackage.svelte'
+  import LoadingPage from '@components/loading-page/LoadingPage.svelte'
   import Box from '@components/_base/box/Box.svelte'
   export const prerender = true
 </script>
 
-<Box mx="large" transparent>
-  <DeployPackage />
-</Box>
+<LoadingPage>
+  <Box mx="large" transparent>
+    <DeployPackage />
+  </Box>
+</LoadingPage>
