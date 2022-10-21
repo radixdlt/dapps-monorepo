214dfad1451482ab9ce30c87d9194df55ab7b9df
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-10-21 04:27:17 +0100

    add lookup transaction test back

---
+++ cypress/e2e/lookup-transaction.cy.ts
@@ -0,0 +1,18 @@
+describe('searching for transaction', () => {
+  it('shows the transaction', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: `**/transaction/*/__data.json`,
+      },
+      {
+        fixture: `transaction.json`
+      }
+    )
+
+    cy.visit('/explorer')
+    cy.get('input').type('ef71a9d6c63444fce6abd2df8fab2755cfb51f6794e578f60d99337193811842')
+    cy.get('form').submit()
+    cy.get('div').should('contain', 'CONFIRMED')
+  })
+})
