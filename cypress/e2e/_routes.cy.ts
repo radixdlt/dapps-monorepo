4421c5ef4a8ba7d6d745ad94492541be6355405e
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-10-22 05:46:42 +0100

    add comment

--- cypress/e2e/_routes.cy.ts
+++ cypress/e2e/_routes.cy.ts
@@ -1,3 +1,5 @@
+// NOTE: we are deliberately running this test first - otherwise other tests fail. 
+
 describe('load all routes', () => {
     it('should load root page', () => {
         cy.visit('/', { failOnStatusCode: true })
