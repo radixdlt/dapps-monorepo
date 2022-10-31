bdde62a358cbcfeb6234dd64cfcc4f822234b3ce
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-10-31 17:10:49 +0900

    update loader

--- src/routes/staking/+page.ts
+++
@@ -1,6 +0,0 @@
-import { queryServer } from '@queries'
-import type { PageServerLoad } from './$types'
-
-export const load: PageServerLoad = async () => ({
-  validators: await queryServer('getValidators')
-})
