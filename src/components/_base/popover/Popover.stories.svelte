7778a7398fc5f456cba2dff34445fe67c5ccf1f9
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-10-31 20:39:14 +0900

    add popover

---
+++ src/components/_base/popover/Popover.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import Popover from './Popover.svelte'
+</script>
+
+<Meta title="Popover" />
+
+<Story name="Primary">
+  <Popover>
+    <button slot="button">Open panel</button>
+    <div slot="content">Panel</div>
+  </Popover>
+</Story>
