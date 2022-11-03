5aaf47a627da49da4944a65b835d7fd26ba04964
AbstractFruitFactory <alexmedkex@gmail.com>
2022-11-03 08:33:37 +0000

    add popover (#120)

---
+++ src/components/_base/popover/Popover.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import {
+    Popover,
+    PopoverButton,
+    PopoverPanel
+  } from '@rgossiaux/svelte-headlessui'
+  import Box from '../box/Box.svelte'
+</script>
+
+<div>
+  <Popover>
+    <PopoverButton as="div">
+      <slot name="button" />
+    </PopoverButton>
+    <PopoverPanel>
+      <Box p="none" mx="none" mt="small" transparent>
+        <slot name="content" />
+      </Box>
+    </PopoverPanel>
+  </Popover>
+</div>
