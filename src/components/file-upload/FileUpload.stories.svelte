0c7d31d973c314344850cdeb24bff60d344987d9
Martin Lichstam <martin.lichstam@gmail.com>
2022-11-03 15:57:29 +0100

    Add remove file file upload

--- src/components/file-upload/FileUpload.stories.svelte
+++ src/components/file-upload/FileUpload.stories.svelte
@@ -2,12 +2,19 @@
   import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
   import { action } from '@storybook/addon-actions'
   import FileUpload from './FileUpload.svelte'
+  const handleRemoveFile = (error, file) => {
+    console.log(file.fileExtension, file)
+    action('onRemoveFile')(error, file)
+  }
 </script>
 
 <Meta title="FileUpload" />
 
 <Template>
-  <FileUpload onAddFile={action('file selected')} />
+  <FileUpload
+    onRemoveFile={handleRemoveFile}
+    onAddFile={action('file selected')}
+  />
 </Template>
 
 <Story name="Primary" />
