2c437a29a3af40aaed677a39300e3a8cc4aac61d
Martin Lichstam <martin.lichstam@gmail.com>
2022-11-15 15:27:02 +0100

    Add toasts where data is fetched/handled

--- src/components/connect-button/ConnectButton.svelte
+++ src/components/connect-button/ConnectButton.svelte
@@ -3,8 +3,17 @@
   import Button from '@components/_base/button/Button.svelte'
   import { query } from '@queries'
   import Box from '@components/_base/box/Box.svelte'
+  import { AlertToast } from '@components/_base/toast/Toasts'
 
   const { get, state } = query('requestAddresses', undefined, { manual: true })
+
+  $: if ($state.status === 'error') {
+    AlertToast({
+      title: 'Error',
+      text: $state.error.message,
+      type: 'error'
+    })
+  }
 
   $: if ($state.status === 'success') {
     accounts.set($state.data.accountAddresses)
