9f179f5f0f3120ad71b3239f38883611c8f1fb89
Martin Lichstam <34944192+lichstam@users.noreply.github.com>
2022-11-17 12:57:27 +0100

    Ptd 250 handle errors in xstate and add toasts (#136)

--- src/components/deploy-package/deploy-package-state-machine.test.ts
+++ src/components/deploy-package/deploy-package-state-machine.test.ts
@@ -15,7 +15,7 @@ describe('#deploy state machine', () => {
   it('Should transition all the way to final', () => {
     const mockToggleMachine = stateMachine.withConfig({
       services: {
-        upload: () => Promise.resolve({ code: 'code', abi: 'abi' }),
+        upload: () => Promise.resolve({ wasm: 'wasm', abi: 'abi' }),
         publish: () => Promise.resolve(),
         getReceipt: () =>
           Promise.resolve({
@@ -26,9 +26,6 @@ describe('#deploy state machine', () => {
       }
     })
     const fetchMachine = interpret(mockToggleMachine).onTransition((state) => {
-      fetchMachine.start()
-      fetchMachine.send('UPLOAD')
-
       if (state.matches('uploaded')) {
         fetchMachine.send('PUBLISH')
       }
@@ -37,5 +34,92 @@ describe('#deploy state machine', () => {
         expect(state.matches('final')).toBeTruthy()
       }
     })
+    fetchMachine.start()
+    fetchMachine.send('UPLOAD')
+  })
+
+  it('Should go to failure state when upload fails', () => {
+    const mockToggleMachine = stateMachine.withConfig({
+      services: {
+        upload: () => Promise.reject(new Error('Upload failed')),
+        publish: () => Promise.resolve(),
+        getReceipt: () =>
+          Promise.resolve({
+            committed: {
+              receipt: { state_updates: { new_global_entities: [] } }
+            }
+          })
+      }
+    })
+    const fetchMachine = interpret(mockToggleMachine).onTransition((state) => {
+      if (state.matches('uploaded')) {
+        fetchMachine.send('PUBLISH')
+      }
+
+      if (state.matches('error')) {
+        try {
+          expect(state.context.error.message).toEqual('Upload failed')
+        } catch (e) {
+          console.log(e)
+        }
+      }
+    })
+    fetchMachine.start()
+    fetchMachine.send('UPLOAD')
+  })
+
+  it('Should error on publish fail', () => {
+    const mockToggleMachine = stateMachine.withConfig({
+      services: {
+        upload: () => Promise.resolve({ wasm: 'wasm', abi: 'abi' }),
+        publish: () => Promise.reject(new Error('Publish failed')),
+        getReceipt: () =>
+          Promise.resolve({
+            committed: {
+              receipt: { state_updates: { new_global_entities: [] } }
+            }
+          })
+      }
+    })
+    const fetchMachine = interpret(mockToggleMachine).onTransition((state) => {
+      if (state.matches('uploaded')) {
+        fetchMachine.send('PUBLISH')
+      }
+
+      if (state.matches('error')) {
+        try {
+          expect(state.context.error.message).toEqual('Publish failed')
+        } catch (e) {
+          console.log(e)
+        }
+      }
+    })
+    fetchMachine.send('UPLOAD')
+    fetchMachine.start()
+  })
+
+  it('Should error on get receipt fail', () => {
+    const mockToggleMachine = stateMachine.withConfig({
+      services: {
+        upload: () => Promise.resolve({ wasm: 'wasm', abi: 'abi' }),
+        publish: () => Promise.resolve(),
+        getReceipt: () => Promise.reject(new Error('Get receipt failed'))
+      }
+    })
+    const fetchMachine = interpret(mockToggleMachine).onTransition((state) => {
+      if (state.matches('uploaded')) {
+        fetchMachine.send('PUBLISH')
+      }
+
+      if (state.matches('error')) {
+        try {
+          expect(state.context.error.message).toEqual('Get receipt failed')
+        } catch (e) {
+          console.log(e)
+        }
+      }
+    })
+    fetchMachine.send('UPLOAD')
+    fetchMachine.start()
   })
 })
