ade4b22b686170210bc3e9b371c7d7b49c805df0
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-11-23 16:20:28 +0000

    feat: remove alphanet gateway

--- src/query-lib/mutations.ts
+++ src/query-lib/mutations.ts
@@ -1,4 +1,3 @@
-import { TransactionApi } from '@radixdlt/alphanet-gateway-api-v0-sdk'
 import { makeQueries } from 'svelte-samlat'
 import { decoders } from '@io'
 import { getWalletSDK } from './'
@@ -21,14 +20,5 @@ export const sendTransaction = makeQueries({
     else throw Error(res.error.message)
   },
   decoder: (res) => decoders('SendTransactionIO', res),
-  transformationFn: (res) => res
-})
-
-export const transactionReceipt = makeQueries({
-  fn: async (intentHash: string) =>
-    new TransactionApi().transactionReceiptPost({
-      v0CommittedTransactionRequest: { intent_hash: intentHash }
-    }),
-  decoder: (res) => decoders('TransactionReceiptIO', res),
   transformationFn: (res) => res
 })
