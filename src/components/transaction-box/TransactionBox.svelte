120a91cb918e35514ce82076cb1515c1c32cd462
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-11-23 16:19:23 +0000

    feat: add gateway sdk package

--- src/components/transaction-box/TransactionBox.svelte
+++ src/components/transaction-box/TransactionBox.svelte
@@ -1,22 +1,9 @@
-<script lang="ts" context="module">
-  type Action = {
-    from: string
-    to: string
-    amount: number
-  }
-
-  export type Transaction = {
-    status: string
-    actions: Action[]
-  }
-</script>
-
 <script lang="ts">
   import Box from '@components/_base/box/Box.svelte'
   import { SkeletonLoader } from '@aleworm/svelte-skeleton-loader'
-  import { shortenAddress } from '@utils'
+  import type { TransactionTransformed } from '@io/gateway'
 
-  export let tx: Transaction | undefined
+  export let tx: TransactionTransformed | undefined
   export let loading: boolean
   const loaderWidth = 100
 </script>
@@ -36,33 +23,6 @@
           <SkeletonLoader width={loaderWidth} />
         {:else if tx}
           {tx.status}
-        {/if}
-      </Box>
-
-      <Box transparent>from</Box>
-      <Box justify="gridEnd" transparent>
-        {#if loading}
-          <SkeletonLoader width={loaderWidth} />
-        {:else if tx}
-          {shortenAddress(tx.actions[0].from)}
-        {/if}
-      </Box>
-
-      <Box transparent>to</Box>
-      <Box transparent justify="gridEnd">
-        {#if loading}
-          <SkeletonLoader width={loaderWidth} />
-        {:else if tx}
-          {shortenAddress(tx.actions[0].to)}
-        {/if}
-      </Box>
-
-      <Box transparent>amount</Box>
-      <Box justify="gridEnd" transparent>
-        {#if loading}
-          <SkeletonLoader width={loaderWidth} />
-        {:else if tx}
-          {tx.actions[0].amount}
         {/if}
       </Box>
     </Box>
