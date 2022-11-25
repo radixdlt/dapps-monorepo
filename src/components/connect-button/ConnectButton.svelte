35caf1154941d355e08f1d32689d74a102765846
Martin Lichstam <martin.lichstam@gmail.com>
2022-11-25 12:52:44 +0000

    feat:add accounts list to connect button

--- src/components/connect-button/ConnectButton.svelte
+++ src/components/connect-button/ConnectButton.svelte
@@ -4,11 +4,10 @@
   import { query } from '@queries'
   import Box from '@components/_base/box/Box.svelte'
   import { AlertToast } from '@components/_base/toast/Toasts'
-  import Icon from '@components/_base/icon/Icon.svelte'
-  import { icons } from '../../icon-assets'
-  import Text from '@components/_base/text/Text.svelte'
   import LoadingSpinner from './LoadingSpinner.svelte'
   import ConnectPopover from './components/ConnectPopover.svelte'
+  import AccountsPopover from './components/AccountsPopover.svelte'
+  import type { Account } from '@io/wallet'
 
   const { get, state } = query('requestAddresses', undefined, { manual: true })
 
@@ -24,6 +23,10 @@
     accounts.set($state.data.oneTimeAccountAddresses)
     selectedAccount.set($state.data.oneTimeAccountAddresses[0])
   }
+
+  const handleSelect = (account: Account) => {
+    selectedAccount.set(account)
+  }
 </script>
 
 <Box transparent p="none">
@@ -32,14 +35,11 @@
       <LoadingSpinner />
     </Button>
   {:else if $state.status === 'success'}
-    <Button connectButton connected size="small" on:click={get}>
-      <Icon
-        height="xs"
-        width="xs"
-        type={icons['dashboard']}
-        filter="brightness(0) invert(1)"
-      /><Text mx="xsmall">Connected</Text>
-    </Button>
+    <AccountsPopover
+      accounts={$accounts}
+      onSelect={handleSelect}
+      selected={$selectedAccount}
+    />
   {:else}
     <ConnectPopover onConnect={get} />
   {/if}
