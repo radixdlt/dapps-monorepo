79c7a86324199d9cc62fec31516d71a5ed52b302
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-11-28 08:19:27 +0000

    feat: add textarea component

---
+++ src/components/_base/textarea/Textarea.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { css } from '@styles'
+
+  export let value: string | undefined = undefined
+  export let placeholder: string | undefined = undefined
+  export let size: 'sm' | 'md' | 'lg' = 'md'
+
+  const style = css({
+    resize: 'none',
+    borderRadius: '$sm',
+    outline: 'none',
+    border: 'none',
+    padding: '$md'
+  })()
+
+  const colsAndRows = {
+    sm: {
+      cols: 15,
+      rows: 2
+    },
+    md: {
+      cols: 30,
+      rows: 4
+    },
+    lg: {
+      cols: 60,
+      rows: 8
+    }
+  }[size]
+</script>
+
+<textarea bind:value class={style} {placeholder} {...colsAndRows} />
