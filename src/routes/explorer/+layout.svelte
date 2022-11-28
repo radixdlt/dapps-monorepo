795d7b0c67473a6a9b2a8d9c63430cf546654bce
Martin Lichstam <martin.lichstam@gmail.com>
2022-11-28 14:26:35 +0000

    feat: add routes for various search types

--- src/routes/explorer/+layout.svelte
+++ src/routes/explorer/+layout.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
-  import { goto } from '$app/navigation'
   import Search from '@components/_base/search/Search.svelte'
   import Box from '@components/_base/box/Box.svelte'
+  import { goto } from '$app/navigation'
+  import { getAddressPrefix } from '@utils'
 
-  let id: string
+  let id = ''
 
-  const search = () => goto(`/explorer/transaction/${id}`)
+  const search = () => {
+    switch (getAddressPrefix(id)) {
+      case 'account':
+        goto(`/explorer/accounts/${id}`)
+        break
+      case 'resource':
+        goto(`/explorer/blocks/${id}`)
+        break
+      case 'transaction':
+        goto(`/explorer/transaction/${id}`)
+        break
+      case 'package':
+        goto(`/explorer/package/${id}`)
+        break
+      case 'component':
+        goto(`/explorer/component/${id}`)
+        break
+      default:
+        goto(`/explorer/transaction/${id}`)
+        break
+    }
+  }
 </script>
 
 <Box my="large" justify="center" transparent>
