ab557782f9494cb7fbf47f2b7e1242a6ee49daf7
Martin Lichstam <34944192+lichstam@users.noreply.github.com>
2022-11-29 17:11:57 +0000

    feat: add infobox for searches (#158)

---
+++ src/components/info-box/InfoBox.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Box from '@components/_base/box/Box.svelte'
+  import { SkeletonLoader } from '@aleworm/svelte-skeleton-loader'
+  import Text from '@components/_base/text/Text.svelte'
+
+  export let data: Record<string, unknown> | undefined
+  export let loading: boolean
+  const loaderWidth = 100
+</script>
+
+<Box>
+  <Box>
+    <Box
+      gap="medium"
+      cx={{
+        display: 'grid',
+        gridTemplateColumns: '2fr 5fr'
+      }}
+      transparent
+    >
+      {#each Object.entries(data || {}) as [key, value]}
+        <Text bold>{key}</Text>
+        <Text>
+          {#if loading}
+            <SkeletonLoader width={loaderWidth} />
+          {:else if value}
+            {value}
+          {/if}
+        </Text>
+      {/each}
+    </Box>
+  </Box>
+</Box>
