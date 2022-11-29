917d9ee2afe93950f8b203c32dbbd4da8b0f3a3b
Martin Lichstam <martin.lichstam@gmail.com>
2022-11-29 15:15:43 +0000

    feat: add infobox for searches

---
+++ src/components/info-box/InfoBox.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import InfoBox from './InfoBox.svelte'
+
+  const data = {
+    status: 'success',
+    from: '0x1234567890',
+    to: '0x1234567890',
+    amount: 100
+  }
+</script>
+
+<Meta title="InfoBox" />
+
+<Story name="Primary">
+  <InfoBox {data} loading={false} />
+</Story>
+
+<Story name="Loading">
+  <InfoBox {data} loading />
+</Story>
