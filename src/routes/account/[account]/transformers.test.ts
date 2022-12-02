0b357e2a828bbf9a7e42cf4b74e7de0e4037531f
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-02 10:33:02 +0000

    feat: show symbol/name in accounts according to order

--- src/routes/account/[account]/transformers.test.ts
+++ src/routes/account/[account]/transformers.test.ts
@@ -21,6 +21,57 @@ describe('#transform for account fetching', () => {
       }
     ]
     const transformedOverview = transformWithOverview(overview)
-    expect(transformedOverview).toEqual({ symbol: '100' })
+    expect(transformedOverview).toEqual({ 'symbol (name)': '100' })
+  })
+
+  it('Should transform according to a prioritzed list', () => {
+    const overview = [
+      {
+        address: '0x1',
+        amount: '100',
+        metadata: {
+          items: [
+            {
+              key: 'name',
+              value: 'name1'
+            },
+            {
+              key: 'symbol',
+              value: 'symbol1'
+            }
+          ]
+        }
+      },
+      {
+        address: '0x2',
+        amount: '200',
+        metadata: {
+          items: [
+            {
+              key: 'symbol',
+              value: 'symbol3'
+            }
+          ]
+        }
+      },
+      {
+        address: '0x3',
+        amount: '300',
+        metadata: {
+          items: [
+            {
+              key: 'name',
+              value: 'name2'
+            }
+          ]
+        }
+      }
+    ]
+    const transformedOverview = transformWithOverview(overview)
+    expect(transformedOverview).toEqual({
+      'symbol1 (name1)': '100',
+      symbol3: '200',
+      name2: '300'
+    })
   })
 })
