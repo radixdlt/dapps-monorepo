9f3749d1e5259c0c1e010648ecf8667e122c2ee3
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-12-06 14:12:58 +0000

    feat: add logo component

---
+++ src/components/logo/Logo.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { goto } from '$app/navigation'
+  import Box from '@components/_base/box/Box.svelte'
+  import Image from '@components/_base/image/Image.svelte'
+</script>
+
+<Box transparent on:click={() => goto('/')} p="none" m="none">
+  <Image
+    cx={{ cursor: 'pointer' }}
+    width="$4xl"
+    alt="logo"
+    src={'/images/dashboard_logo.svg'}
+  />
+</Box>
