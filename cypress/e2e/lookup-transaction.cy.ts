c11082f76449e85480c50063cf3867e42a05af24
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-07 10:36:42 +0100

    Fix e2etests

--- cypress/e2e/lookup-transaction.cy.ts
+++ cypress/e2e/lookup-transaction.cy.ts
@@ -1,18 +1,21 @@
 describe('searching for transaction', () => {
-    it('shows the transaction', () => {
-      cy.intercept(
-        {
-          method: 'POST',
-          url: `**/transaction/status`,
-        },
-        {
-          fixture: `transaction.json`
-        }
-      )
-  
-      cy.visit('/explorer')
-      cy.get('input').type('0617e513d01c2676dd23382e7dfa595bc3963591a05de4b0c049e18402aaccee')
-      cy.get('form').submit()
-      cy.get('div').should('contain', 'succeeded')
-    })
+  it('shows the transaction', () => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: `**/transaction/status`
+      },
+      {
+        fixture: `transaction.json`
+      }
+    )
+
+    cy.visit('/')
+    cy.get('input').type(
+      '0617e513d01c2676dd23382e7dfa595bc3963591a05de4b0c049e18402aaccee'
+    )
+    cy.get('form').submit()
+    cy.get('div').should('contain', 'succeeded')
   })
+})
+
