02274c24194a1ddc2cfdc5999aabbac8589e1ab4
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-07 14:18:04 +0100

    Revert "Remove old mock sdk"
    This reverts commit af4d6f06a80ead12d177bd3dabf3a1f322d7b524.

---
+++ mock-sdk/build/wallet.js
@@ -0,0 +1,25 @@
+"use strict";
+exports.__esModule = true;
+exports.Transaction = exports.Wallet = void 0;
+var neverthrow_1 = require("neverthrow");
+exports.Wallet = {
+    isAvailable: function () { return true; },
+    connect: function (name, info) { return (0, neverthrow_1.okAsync)({
+        accounts: [
+            {
+                label: 'Main',
+                address: 'rdx123'
+            },
+            {
+                label: 'NFTs',
+                address: 'rdx456'
+            }
+        ]
+    }); },
+    requestInfo: function () { },
+    requestProof: function () { },
+    sendTransaction: function (manifest) { }
+};
+var Transaction = function (instructions) { };
+exports.Transaction = Transaction;
+//# sourceMappingURL=wallet.js.map
