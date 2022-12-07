15e0d79a57ec056b1fa18847d197d983196d01a3
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-07 08:49:36 +0100

    Add search bar to header and remove dashboard from side menu

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -3,6 +3,34 @@
   import ThemeSwitch from '@components/_base/switch/theme-switch/ThemeSwitch.svelte'
   import ConnectButton from '@components/connect-button/ConnectButton.svelte'
   import Logo from '@components/logo/Logo.svelte'
+  import { getAddressPrefix } from '@utils'
+  import Search from '@components/_base/search/Search.svelte'
+  import { goto } from '$app/navigation'
+
+  let address = ''
+
+  const search = () => {
+    switch (getAddressPrefix(address)) {
+      case 'account':
+        goto(`/explorer/account/${address}`)
+        break
+      case 'resource':
+        goto(`/explorer/blocks/${address}`)
+        break
+      case 'transaction':
+        goto(`/explorer/transaction/${address}`)
+        break
+      case 'package':
+        goto(`/explorer/package/${address}`)
+        break
+      case 'component':
+        goto(`/explorer/component/${address}`)
+        break
+      default:
+        goto(`/explorer/transaction/${address}`)
+        break
+    }
+  }
 </script>
 
 <Box
@@ -17,9 +45,13 @@
   justify="between"
 >
   <Logo />
+  <Box cx={{ flex: 0.8 }} p="none">
+    <form on:submit|preventDefault={search}>
+      <Search bind:value={address} placeholder="Enter Transaction ID" />
+    </form>
+  </Box>
   <Box cx={{ gap: '$md' }} p="none" flex="row" items="center">
     <ConnectButton />
-
     <ThemeSwitch />
   </Box>
 </Box>
