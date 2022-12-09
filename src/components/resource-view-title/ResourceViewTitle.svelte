e12d88826ed578aa418cf14d6e5377f6ff00e3c8
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-12-09 15:26:38 +0000

    feat: add ResourceViewTitle component

---
+++ src/components/resource-view-title/ResourceViewTitle.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import IconTextItem from '@components/icon-text-item/IconTextItem.svelte'
+  import Box from '@components/_base/box/Box.svelte'
+  import Text from '@components/_base/text/Text.svelte'
+  import { shortenAddress } from '@utils'
+
+  export let title: string
+  export let resourceAddress: string
+</script>
+
+<Text inline size="xxlarge" bold>{title}</Text>
+<Box transparent m="none" p="none" inline>
+  <IconTextItem
+    on:click={() => navigator.clipboard.writeText(resourceAddress)}
+    icon="copy"
+    interactiveIcon
+    iconPosition="right"
+    iconSize="xs"
+    noPadding
+    textColor="$highlightedText">{shortenAddress(resourceAddress)}</IconTextItem
+  >
+</Box>
