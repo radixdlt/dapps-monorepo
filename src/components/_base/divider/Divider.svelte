cc060714698ef1ab794fdaaf05f2cf9608d2f87f
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-12-12 05:47:58 +0000

    feat: add divider

---
+++ src/components/_base/divider/Divider.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { css } from '@styles'
+
+  const style = css({
+    borderRadius: '$sm',
+    borderTop: '1px solid black',
+    borderColor: '$grey4'
+  })()
+</script>
+
+<hr class={style} />
