af3be24f18883c4fde0a27ff1f2c022a359005ee
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-12 16:38:26 +0100

    feat: add tabs

---
+++ src/components/_base/tabs/Tabs.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Tabs from './Tabs.svelte'
+
+  const tabs = [
+    { name: 'One of my accounts', content: 'content1' },
+    { name: 'Other account', content: 'content2' }
+  ]
+</script>
+
+<Meta title="Tabs" component={Tabs} />
+
+<Template>
+  <Tabs {tabs} />
+</Template>
+
+<Story name="Primary" />
