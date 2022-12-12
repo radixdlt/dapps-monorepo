3107d4acb5943e334512d16188794a23f6290d7b
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-12 10:46:16 +0100

    feat: add cookie banner in prod

--- src/feature-flags/app-flags.prod.ts
+++ src/feature-flags/app-flags.prod.ts
@@ -3,5 +3,10 @@ export const prodAppFlags = [
     id: 'dark-mode',
     description: 'Display the toggle for dark mode',
     enabled: false
+  },
+  {
+    id: 'cookie-banner',
+    description: 'Display cookie banner from one trust',
+    enabled: true
   }
 ] as const
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { featureFlags } from '../feature-flags'
+</script>
+
+<svelte:head>
+  {#if featureFlags().getFlag('cookie-banner')?.enabled}
+    <!-- OneTrust Cookies Consent Notice start for betanet-dashboard.radixdlt.com -->
+    <script
+      src="https://cdn-ukwest.onetrust.com/scripttemplates/otSDKStub.js"
+      type="text/javascript"
+      charset="UTF-8"
+      data-domain-script="de2483c5-9867-4af6-9434-dfc97b731f0c"
+    ></script>
+    <script type="text/javascript">
+      function OptanonWrapper() {}
+    </script>
+    <!-- OneTrust Cookies Consent Notice end for betanet-dashboard.radixdlt.com -->
+  {/if}
+</svelte:head>
