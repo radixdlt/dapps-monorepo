7a2e5b9eb34942dbf6a2fd70389f7088397c79a8
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-12 10:46:16 +0100

    feat: add cookie banner in prod

--- src/feature-flags/app-flags.dev.ts
+++ src/feature-flags/app-flags.dev.ts
@@ -3,5 +3,10 @@ export const devAppFlags = [
     id: 'dark-mode',
     description: 'Display the toggle for dark mode',
     enabled: true
+  },
+  {
+    id: 'cookie-banner',
+    description: 'Display cookie banner from one trust',
+    enabled: false
   }
 ] as const
