1c9d1bb88e199f6ec9bdc44fc540f3f705f22dea
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-12 16:56:43 +0100

    feat: add text input

---
+++ src/components/_base/input/Input.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Input from './Input.svelte'
+</script>
+
+<Meta title="Input" component={Input} />
+
+<Template>
+  <Input placeholder="Placeholder" />
+</Template>
+
+<Story name="Primary" />
---
+++ src/components/_base/input/Input.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { css } from '@styles'
+
+  export let value: string | undefined = undefined
+  export let placeholder: string | undefined = undefined
+
+  const style = css({
+    width: '100%',
+    borderRadius: '$sm',
+    backgroundColor: '$transparent',
+    padding: '$sm',
+    border: 'none',
+    borderBottom: '1px solid $borderColor',
+    color: '$textColor'
+  })()
+</script>
+
+<input bind:value class={style} {placeholder} />
