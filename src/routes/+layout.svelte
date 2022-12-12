21090a76febbf7f13f29d6558d7995af5669c796
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-12 14:38:54 +0100

    fix: cookie present in sublinks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { featureFlags } from '../feature-flags'
   import Header from '@components/header/Header.svelte'
   import { darkTheme, getCssText } from '@styles'
   import { navigating, page } from '$app/stores'
@@ -22,6 +23,22 @@
   }
   navigating
 </script>
+
+<svelte:head>
+  {#if featureFlags().getFlag('cookie-banner')?.enabled}
+    <!-- OneTrust Cookies Consent Notice start for betanet-dashboard.radixdlt.com -->
+    <script
+      src="https://cdn-ukwest.onetrust.com/scripttemplates/otSDKStub.js"
+      type="text/javascript"
+      charset="UTF-8"
+      data-domain-script="de2483c5-9867-4af6-9434-dfc97b731f0c"
+    ></script>
+    <script type="text/javascript">
+      function OptanonWrapper() {}
+    </script>
+    <!-- OneTrust Cookies Consent Notice end for betanet-dashboard.radixdlt.com -->
+  {/if}
+</svelte:head>
 
 <!-- enables SSR of css -->
 {@html `<${''}style id="stitches">${getCssText()}</${''}style>`}
