8ff5611c8cd1f95c195d1c298604b753867e4d50
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-13 17:44:15 +0100

    fix: add feature flags for all items in sidebar menu itself

--- src/components/sidebar-with-navbar/SidebarWithNavbar.svelte
+++ src/components/sidebar-with-navbar/SidebarWithNavbar.svelte
@@ -3,6 +3,7 @@
   import Sidebar from '@components/_base/sidebar/Sidebar.svelte'
   import type { Page } from '@sveltejs/kit'
   import { isSameRoute } from '@utils'
+  import { featureFlags } from '@featureFlags'
   import type { icons } from 'src/icon-assets'
 
   export let page: Page<Record<string, string>> = {} as any // TODO: fix this after storybook gets typesupport
@@ -11,28 +12,38 @@
     text: string
     icon: keyof typeof icons
     path: string
+    isVisible?: boolean
   }> = [
-    { text: 'Deploy Package', icon: 'dashboard', path: '/deploy-package' },
+    {
+      text: 'Deploy Package',
+      icon: 'dashboard',
+      path: '/deploy-package',
+      isVisible: featureFlags().getFlag('deploy-package')?.enabled
+    },
     {
       text: 'Transaction Manifest',
       icon: 'transactions',
-      path: '/transaction-manifest'
+      path: '/transaction-manifest',
+      isVisible: featureFlags().getFlag('transaction-manifest')?.enabled
     },
     {
       text: 'Send Tokens',
       icon: 'tokens',
-      path: '/send-tokens'
+      path: '/send-tokens',
+      isVisible: featureFlags().getFlag('send-tokens')?.enabled
     }
   ]
 </script>
 
 <Sidebar disableClickOutside show>
   {#each routes as route (route.path)}
-    <SidebarItem
-      icon={route.icon}
-      isActive={isSameRoute(route.path, page.routeId ?? '')}
-      link={route.path}
-      on:click={() => (page.routeId = route.path)}>{route.text}</SidebarItem
-    >
+    {#if route.isVisible}
+      <SidebarItem
+        icon={route.icon}
+        isActive={isSameRoute(route.path, page.routeId ?? '')}
+        link={route.path}
+        on:click={() => (page.routeId = route.path)}>{route.text}</SidebarItem
+      >
+    {/if}
   {/each}
 </Sidebar>
