289cee95240474cdbbe81c31abac376748a1d9ad
Martin Lichstam <martin.lichstam@gmail.com>
2022-12-13 17:42:47 +0100

    feat: add feature flags for sidebar items

--- src/feature-flags/app-flags.dev.ts
+++ src/feature-flags/app-flags.dev.ts
@@ -1,5 +1,20 @@
 export const devAppFlags = [
   {
+    id: 'deploy-package',
+    description: 'Deploy package in the sidebar menu',
+    enabled: true
+  },
+  {
+    id: 'transaction-manifest',
+    description: 'Transaction Manifest in the sidebar menu',
+    enabled: true
+  },
+  {
+    id: 'send-tokens',
+    description: 'Send Tokens in the sidebar menu',
+    enabled: true
+  },
+  {
     id: 'dark-mode',
     description: 'Display the toggle for dark mode',
     enabled: true
