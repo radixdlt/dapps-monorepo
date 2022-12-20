7554d48a6e3de62f84f43d417791f9066d67df5a
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-12-20 12:09:14 +0000

    feat: add success screen for tx manifest

--- src/routes/transaction-manifest/+page.svelte
+++ src/routes/transaction-manifest/+page.svelte
@@ -5,41 +5,63 @@
   import Text from '@components/_base/text/Text.svelte'
   import Textarea from '@components/_base/textarea/Textarea.svelte'
   import { mutate } from '@queries'
+  import { AlertToast } from '@components/_base/toast/Toasts'
+  import Success from './Success.svelte'
 
   let transactionManifest = ''
 
-  const { trigger, loading } = mutate('sendTransaction')
+  const { trigger, loading, data, error } = mutate('sendTransaction')
+
+  $: if ($data?.transactionIntentHash) {
+    AlertToast({
+      title: 'Success!',
+      text: `TxHash: ${$data.transactionIntentHash}`,
+      type: 'success'
+    })()
+  }
+
+  $: if ($error) {
+    AlertToast({
+      title: $error.name,
+      text: $error.message,
+      type: 'error'
+    })()
+  }
 </script>
 
-<Box transparent>
-  <Text size={'xxlarge'} bold>Send Raw Transaction</Text>
-</Box>
-<Box transparent>
-  <Text
-    >Enter raw transaction manifest text to send to your linked Radix Wallet. No
-    method call to “lock_fee” is required – the wallet will add this
-    automatically.</Text
-  >
-</Box>
-<Box justify="center" transparent>
-  <Textarea
-    bind:value={transactionManifest}
-    placeholder="Enter a raw transaction manifest"
-    size="lg"
-  />
-</Box>
-
-<Box justify="center" transparent>
-  {#if $loading}
-    <Button>
-      <LoadingSpinner />
-    </Button>
-  {:else}
-    <Button
-      on:click={() =>
-        trigger({
-          transactionManifest
-        })}>Submit</Button
+{#if $data?.transactionIntentHash}
+  <Success txID={$data?.transactionIntentHash} />
+{:else}
+  <Box transparent>
+    <Text size={'xxlarge'} bold>Send Raw Transaction</Text>
+  </Box>
+  <Box transparent>
+    <Text
+      >Enter raw transaction manifest text to send to your linked Radix Wallet.
+      No method call to “lock_fee” is required – the wallet will add this
+      automatically.</Text
     >
-  {/if}
-</Box>
+  </Box>
+  <Box justify="center" transparent>
+    <Textarea
+      bind:value={transactionManifest}
+      placeholder="Enter a raw transaction manifest"
+      size="lg"
+    />
+  </Box>
+
+  <Box justify="center" transparent>
+    {#if $loading}
+      <Button>
+        <LoadingSpinner />
+      </Button>
+    {:else}
+      <Button
+        on:click={() =>
+          trigger({
+            transactionManifest
+          })}>Submit</Button
+      >
+    {/if}
+  </Box>
+{/if}
