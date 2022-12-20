c11c287de418e7336c95eab987f2333b38c2ca14
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2022-12-20 05:31:47 +0000

    fix: account view nft links

--- src/routes/resource/[resource]/+page.ts
+++ src/routes/resource/[resource]/+page.ts
@@ -4,7 +4,10 @@ import type { PageLoad } from './$types'
 export const prerender = false
 
 export const load: PageLoad = async ({ params }) => ({
-  state: query('getEntityDetails', params.resource, {
+  entityDetailsState: query('getEntityDetails', params.resource, {
+    manual: false
+  }).state,
+  nonFungiblesState: query('getNonFungibleIDs', params.resource, {
     manual: false
   }).state
 })
--- src/state-machines/transformers.ts
+++ src/state-machines/transformers.ts
@@ -1,4 +1,5 @@
 import type { EntityOverviewTransformed } from '@queries/transformations'
+import { getNFTAddress } from '@utils'
 
 export type TransformWithOverview = ReturnType<typeof transformWithOverview>
 
@@ -27,6 +28,38 @@ export const transformWithOverview = (
     }
 
     acc.push(next)
+
+    return acc
+  }, [])
+  return transformedOverview
+}
+
+export const transformNFTWithOverview = (
+  overview?: Array<
+    EntityOverviewTransformed['withOverviews'][0] & {
+      non_fungible_ids: {
+        items: Array<{
+          non_fungible_id: string
+        }>
+      }
+    }
+  >
+) => {
+  const transformedOverview = overview?.reduce<
+    Array<{ key: string; value?: string; address: string }>
+  >((acc, overview) => {
+    const name = overview.metadata.items?.find(
+      (item) => item.key === 'name'
+    )?.value
+
+    overview.non_fungible_ids.items.forEach(({ non_fungible_id }) =>
+      acc.push({
+        key: name
+          ? `(${name}) ${getNFTAddress(overview.address, non_fungible_id)}`
+          : `${getNFTAddress(overview.address, non_fungible_id)}`,
+        address: `${overview.address}:${non_fungible_id}`
+      })
+    )
 
     return acc
   }, [])
