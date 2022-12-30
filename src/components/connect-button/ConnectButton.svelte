3d0d4600a8db7e23ef954ff961b87b02ac56e99b
javier-rdx <javier.rojo@rdx.works>
2022-12-30 12:56:17 +0100

    feat: bump connect-button, use onMount for wrapper

--- src/components/connect-button/ConnectButton.svelte
+++ src/components/connect-button/ConnectButton.svelte
@@ -1,34 +1,41 @@
 <script lang="ts">
   import { networkConfig } from '@constants'
   import { accounts, selectedAccount } from '@stores'
+  import { onMount } from 'svelte'
   import { configureConnectButton } from '../../wallet-sdk'
 
-  const { destroy } = configureConnectButton({
-    dAppId: 'dashboard',
-    logLevel: 'DEBUG',
-    networkId: networkConfig?.id,
-    onConnect: async ({ setState, getWalletData }) => {
-      setState({ loading: true, connected: false })
+  onMount(() => {
+    const { destroy } = configureConnectButton({
+      dAppId: 'dashboard',
+      logLevel: 'DEBUG',
+      networkId: networkConfig?.id,
+      initialState: {
+        loading: false,
+        connected: false
+      },
+      onConnect: async ({ setState, getWalletData }) => {
+        setState({ loading: true, connected: false })
 
-      const response = await getWalletData({
-        oneTimeAccountsWithoutProofOfOwnership: {}
-      })
+        const response = await getWalletData({
+          oneTimeAccountsWithoutProofOfOwnership: {}
+        })
 
-      if (response.isOk()) {
-        accounts.set(response.value.oneTimeAccounts)
-        selectedAccount.set(response.value.oneTimeAccounts[0])
-        setState({ loading: false, connected: true })
-      } else {
+        if (response.isOk()) {
+          accounts.set(response.value.oneTimeAccounts)
+          selectedAccount.set(response.value.oneTimeAccounts[0])
+          setState({ loading: false, connected: true })
+        } else {
+          setState({ loading: false, connected: false })
+        }
+      },
+      onDisconnect: ({ setState }) => {
         setState({ loading: false, connected: false })
-      }
-    },
-    onDisconnect: ({ setState }) => {
-      setState({ connected: false })
-      accounts.set(undefined)
-      selectedAccount.set(undefined)
-    },
-    // clean up dangling subscriptions when HMR is triggered
-    onDestroy: () => destroy()
+        accounts.set(undefined)
+        selectedAccount.set(undefined)
+      },
+      // clean up dangling subscriptions when HMR is triggered
+      onDestroy: () => destroy()
+    })
   })
 </script>
 
