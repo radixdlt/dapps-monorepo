f2447e2b14ffd55a18629b439d8b24d6503ed771
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-01-11 11:38:54 +0000

    feat: implement component view

--- src/routes/component/[component]/+page.svelte
+++ src/routes/component/[component]/+page.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
+  import InfoBox from '@components/info-box/InfoBox.svelte'
+  import ResourceViewTitle from '@components/resource-view-title/ResourceViewTitle.svelte'
+  import Box from '@components/_base/box/Box.svelte'
+  import Card from '@components/_base/card/Card.svelte'
+  import { query } from '@queries'
   import type { PageData } from './$types'
 
   export let data: PageData
 
   $: componentAddress = data.componentAddress
+
+  $: ({ state } = query('getEntityDetails', componentAddress, {
+    manual: false
+  }))
 </script>
+
+<Box>
+  <ResourceViewTitle title="Package" resourceAddress={componentAddress} />
+</Box>
+
+<Box>
+  <Card>
+    <Box wrapper slot="body">
+      {#if $state.status === 'loading'}
+        <InfoBox loading />
+      {:else}
+        <InfoBox entries={$state.data?.metadata.items} />
+      {/if}
+    </Box>
+  </Card>
+</Box>
