c51cb9871ebdc325ed46f5eaf3abdb40dd3bfaf0
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-01-17 23:48:22 +0000

    feat: add checkbox

---
+++ src/components/_base/checkbox/Checkbox.stories.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Checkbox from './Checkbox.svelte'
+
+  const options = [
+    {
+      name: 'Bob',
+      label: 'Bob'
+    },
+    {
+      name: 'Kirby',
+      label: 'Kirby'
+    }
+  ]
+</script>
+
+<Meta title="Checkbox" component={Checkbox} />
+<Template>
+  <Checkbox {options} />
+</Template>
+
+<Story name="Primary" />
---
+++ src/components/_base/checkbox/Checkbox.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { css } from '@styles'
+  import Box from '../box/Box.svelte'
+
+  type G = $$Generic
+
+  type T = G & { label: string }
+
+  export let options: T[]
+  export let selected: G[] = []
+
+  const inputStyle = css({
+    appearance: 'none',
+    width: '$xs',
+    height: '$xs',
+    border: '0.1em solid $muted',
+    marginRight: '$sm',
+    transform: 'translateY(0.15em)',
+    '&:checked': {
+      backgroundColor: '$secondary'
+    }
+  })()
+</script>
+
+<Box wrapper flex="col">
+  {#each options as option}
+    <label>
+      <input
+        class={inputStyle}
+        type="checkbox"
+        bind:group={selected}
+        value={option}
+      />
+      {option.label}
+    </label>
+  {/each}
+</Box>
