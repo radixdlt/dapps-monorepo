9677f4fdb6eba0346bc4d7c7a3762084659e46c9
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-01-20 11:06:13 +0000

    feat: add send nft page

--- src/components/navbar-page/NavbarPage.svelte
+++ src/components/navbar-page/NavbarPage.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
   import Box from '@components/_base/box/Box.svelte'
   import Text from '@components/_base/text/Text.svelte'
-  import { useMachine } from '@xstate/svelte'
-  import { stateMachine } from './navbar-page-state-machine'
   import { accounts } from '@stores'
   import { shortenAddress } from '@utils'
 
   export let title: string
 
-  const { state, send } = useMachine(stateMachine)
+  let loggedIn = false
 
-  $: if ($accounts && $accounts.length > 0) {
-    send('LOGGEDIN')
-  }
+  $: if ($accounts && $accounts.length > 0) loggedIn = true
 
   $: accountsList = $accounts?.map((account) => ({
     address: account.address,
@@ -23,15 +19,11 @@
 
 <Box>
   <Text inline size="xxlarge" mb="medium" bold>{title}</Text>
-  {#if $state.matches('not-logged-in')}
+  {#if !loggedIn}
     <Text bold>Please connect your Radix Wallet to get started.</Text>
   {/if}
-  {#if $state.matches('idle')}
+
+  {#if loggedIn}
     <slot accounts={accountsList} />
-  {/if}
-  {#if $state.matches('error')}
-    <Text inline size="large" mb="medium" bold>
-      Error: {$state.context.error.message}
-    </Text>
   {/if}
 </Box>
