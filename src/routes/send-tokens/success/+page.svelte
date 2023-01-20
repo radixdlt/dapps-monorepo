2e8046d4b686fcd22591da613302bb91d544076d
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-01-20 11:05:00 +0000

    refactor: move success component

--- src/routes/send-tokens/success/+page.svelte
+++ src/routes/send-tokens/success/+page.svelte
@@ -1,31 +1,8 @@
 <script lang="ts">
-  import InfoBox from '@components/info-box/InfoBox.svelte'
-  import Box from '@components/_base/box/Box.svelte'
-  import Text from '@components/_base/text/Text.svelte'
+  import Success from '@components/send-tokens/Success.svelte'
   import type { PageData } from './$types'
 
   export let data: PageData
 </script>
 
-<center>
-  <Text bold size="xlarge">Transfer successful! 🎉</Text>
-  <Box cx={{ width: '900px' }}>
-    <InfoBox
-      entries={[
-        {
-          key: 'Tx ID',
-          value: data.txID
-        }
-      ]}
-      loading={false}
-      transparent
-    >
-      <Text align="right" bold slot="key" let:entry>
-        {entry.key}
-      </Text>
-      <Text align="left" color="link" underlined slot="value" let:entry>
-        <a href="/transaction/{entry.value}">{entry.value}</a>
-      </Text>
-    </InfoBox>
-  </Box>
-</center>
+<Success txID={data.txID} />
