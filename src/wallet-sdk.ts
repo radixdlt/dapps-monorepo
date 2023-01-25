514698813880155c13ce9e7d33c1755f7b0e5456
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-01-25 08:13:45 +0000

    refactor: send transaction

--- src/wallet-sdk.ts
+++ src/wallet-sdk.ts
@@ -4,34 +4,43 @@ import { getTxIdFromMessage } from '@utils'
 import { pipe } from 'ramda'
 export { configure as configureConnectButton } from '@radixdlt/connect-button'
 
-export const getWalletData = () => getMethods().getWalletData
-
-export const sendTransaction = (toastOptions?: {
+type ToastOptions = {
   successText?: string
   errorText?: (errorMessage?: string) => string
-}) =>
-  pipe(getMethods().sendTransaction, (asyncResult) =>
-    asyncResult
+}
+
+export const getWalletData = () => getMethods().getWalletData
+
+const showSuccessToast = (
+  transactionIntentHash: string,
+  options?: ToastOptions
+) =>
+  AlertToast({
+    title: 'Transaction successful!',
+    text: options?.successText ?? '',
+    txId: transactionIntentHash,
+    type: 'success'
+  })
+
+const showErrorToast = (err: Partial<Error>, options?: ToastOptions) =>
+  AlertToast({
+    title: 'Transaction failed.',
+    text: options?.errorText
+      ? options?.errorText(err.message)
+      : err.message ?? '',
+    txId: err.message ? getTxIdFromMessage(err.message) : undefined,
+    type: 'error'
+  })
+
+export const sendTransaction = (toastOptions?: ToastOptions) =>
+  pipe(getMethods().sendTransaction, (result) =>
+    result
       .map(({ transactionIntentHash }) => {
-        AlertToast({
-          title: 'Transaction successful!',
-          text: toastOptions?.successText ?? '',
-          txId: transactionIntentHash,
-          type: 'success'
-        })
-        return {
-          transactionIntentHash
-        }
+        showSuccessToast(transactionIntentHash, toastOptions)
+        return { transactionIntentHash }
       })
       .mapErr((err) => {
-        AlertToast({
-          title: 'Transaction failed.',
-          text: toastOptions?.errorText
-            ? toastOptions?.errorText(err.message)
-            : err.message ?? '',
-          txId: err.message ? getTxIdFromMessage(err.message) : undefined,
-          type: 'error'
-        })
+        showErrorToast(err, toastOptions)
         return err
       })
   )
