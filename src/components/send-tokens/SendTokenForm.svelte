d1c018c41b4db751cf0c6586f54bb85c1e123b91
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-01-26 15:45:03 +0000

    refactor: remove tabs from send tokens

--- src/components/send-tokens/SendTokenForm.svelte
+++ src/components/send-tokens/SendTokenForm.svelte
@@ -12,10 +12,7 @@
   import Box from '@components/_base/box/Box.svelte'
   import Text from '@components/_base/text/Text.svelte'
   import Select, { type Options } from '@components/_base/select/Select.svelte'
-  import Tabs from '@components/_base/tabs/Tabs.svelte'
-  import TabPanel from '@components/_base/tabs/TabPanel.svelte'
   import Input from '@components/_base/input/Input.svelte'
-  import Tab from '@components/_base/tabs/Tab.svelte'
   import Button from '@components/_base/button/Button.svelte'
   import LoadingSpinner from '@components/_base/button/loading-spinner/LoadingSpinner.svelte'
   import { stateMachine } from '@stateMachines/account-state-machine'
@@ -54,6 +51,8 @@
     (resourceSelected = selected)
 
   $: if ($data) goto(`/send-nft/success?txID=${$data.transactionIntentHash}`)
+
+  let toAccount: 'my' | 'other' = 'my'
 </script>
 
 <Box bgColor="surface" flex="col" gap="medium">
@@ -68,42 +67,45 @@
   </Box>
   <Box bgColor="surface" mt="medium" cx={boxStyle}>
     <Text bold align="right">To</Text>
-    <Tabs>
-      <svelte:fragment slot="tabs">
-        <Tab _default />
-        <Box
-          bgColor="surface"
-          inline
-          p="none"
-          cx={{ marginRight: '$lg', marginLeft: '$sm' }}>One of My Accounts</Box
-        >
-        <Tab />
-        <Box
-          bgColor="surface"
-          inline
-          p="none"
-          cx={{ marginRight: '$lg', marginLeft: '$sm' }}>Other account</Box
-        >
-      </svelte:fragment>
-      <svelte:fragment slot="panels">
-        <TabPanel>
-          <Box bgColor="surface" px="none" cx={{ width: '300px' }}>
-            <Select
-              placeholder="Select personal account"
-              handleSelect={(account) => (selectedToAccount = account)}
-              options={accounts}
-            />
-          </Box>
-        </TabPanel>
-        <TabPanel
-          ><Input
-            type="text"
-            bind:value={otherAccount}
-            placeholder="Account Address"
-          /></TabPanel
-        >
-      </svelte:fragment>
-    </Tabs>
+    <Box wrapper>
+      <input type="radio" name="toAccount" bind:group={toAccount} value="my" />
+      <Box
+        bgColor="surface"
+        inline
+        p="none"
+        cx={{ marginRight: '$lg', marginLeft: '$sm' }}>One of My Accounts</Box
+      >
+      <input
+        type="radio"
+        name="toAccount"
+        bind:group={toAccount}
+        value="other"
+      />
+      <Box
+        bgColor="surface"
+        inline
+        p="none"
+        cx={{ marginRight: '$lg', marginLeft: '$sm' }}>Other account</Box
+      >
+    </Box>
+    <div />
+    {#if toAccount === 'my'}
+      <Box bgColor="surface" px="none" cx={{ width: '300px' }}>
+        <Select
+          placeholder="Select personal account"
+          handleSelect={(account) => (selectedToAccount = account)}
+          options={accounts}
+        />
+      </Box>
+    {/if}
+
+    {#if toAccount === 'other'}
+      <Input
+        type="text"
+        bind:value={otherAccount}
+        placeholder="Account Address"
+      />
+    {/if}
   </Box>
   {#if tokenType === 'fungible'}
     <SendFungible
