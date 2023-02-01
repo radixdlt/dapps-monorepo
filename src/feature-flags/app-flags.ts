944cdaf378dd45d7602cf75858cd62d3695ae6ed
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-02-01 08:37:17 +0000

    Revert "Revert "refactor: app flags""
    This reverts commit 8d2116e3b6a828d9302108f923d13071372e75c2.

--- src/feature-flags/app-flags.ts
+++ src/feature-flags/app-flags.ts
@@ -1,2 +1,44 @@
-export { devAppFlags } from './app-flags.dev'
-export { prodAppFlags } from './app-flags.prod'
+const environment = {
+  PROD: 'prod',
+  DEV: 'dev'
+}
+
+export const appFlags = (env: keyof typeof environment) =>
+  [
+    {
+      id: 'deploy-package',
+      description: 'Deploy package in the sidebar menu',
+      enabled: true
+    },
+    {
+      id: 'transaction-manifest',
+      description: 'Transaction Manifest in the sidebar menu',
+      enabled: true
+    },
+    {
+      id: 'send-tokens',
+      description: 'Send Tokens in the sidebar menu',
+      enabled: true
+    },
+    {
+      id: 'send-nft',
+      description: 'Send NFTs in the sidebar menu',
+      enabled: true
+    },
+    {
+      id: 'dark-mode',
+      description: 'Display the toggle for dark mode',
+      enabled: {
+        [environment.DEV]: true,
+        [environment.PROD]: false
+      }[environment[env]]
+    },
+    {
+      id: 'cookie-banner',
+      description: 'Display cookie banner from one trust',
+      enabled: {
+        [environment.DEV]: false,
+        [environment.PROD]: true
+      }[environment[env]]
+    }
+  ] as const
