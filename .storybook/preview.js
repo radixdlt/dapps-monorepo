7b9f826670bb3a0febb12a91aa0dc157157ece9a
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-02-23 08:30:45 +0000

    feat: add storybook testing for Account

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
-import ThemeDecorator from './ThemeDecorator.svelte';
-
-export const decorators = [(_, story) => ({ Component: ThemeDecorator, props: { context: story.globals } })];
+// import ThemeDecorator from './ThemeDecorator.svelte';
+// export const decorators = [(_, story) => ({ Component: ThemeDecorator, props: { context: story.globals } })];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
---
+++ src/components/_search-pages/account/Account.stories.ts
@@ -0,0 +1,97 @@
+import { within } from '@storybook/testing-library'
+import Account from './Account.svelte'
+
+const metadata = {
+  items: [
+    {
+      key: 'name',
+      value: 'Test'
+    },
+    {
+      key: 'description',
+      value: `this is a description
+with multiple lines`
+    },
+    {
+      key: 'other',
+      value: 'thing'
+    }
+  ]
+}
+
+export default {
+  title: 'Search Pages/Account',
+  component: Account
+}
+
+const Template = (args: any) => ({
+  Component: Account,
+  props: {
+    address: 'rdx_000000',
+    ...args
+  }
+})
+
+export const WithoutResources = Template.bind(null, {
+  resources: new Promise((resolve) =>
+    resolve({
+      fungible: [],
+      nonFungible: []
+    })
+  ),
+  details: new Promise((resolve) => resolve({ metadata }))
+})
+
+export const WithResources = Template.bind(null, {
+  resources: new Promise((resolve) =>
+    resolve({
+      fungible: [
+        {
+          address: 'rdx_10000000000',
+          key: 'XRD',
+          value: '100'
+        },
+        {
+          address: 'rdx_10000000000',
+          key: 'BTC',
+          value: '190000000'
+        }
+      ],
+      nonFungible: [
+        {
+          address: 'rdx_10000000000',
+          key: 'FLOOP (Test) 0x0000000:#1#'
+        },
+        {
+          address: 'rdx_10000000000',
+          key: 'SMARF (Test) 0x0000000:#1#'
+        },
+        {
+          address: 'rdx_10000000000',
+          key: 'PERP (Test) 0x0000000:#12#'
+        }
+      ]
+    })
+  ),
+  details: new Promise((resolve) => resolve({ metadata }))
+})
+
+export const Loading = Template.bind(null, {
+  resources: new Promise(() => {}),
+  details: new Promise(() => {})
+})
+
+// @ts-ignore
+WithoutResources.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const { findByText } = canvas
+  await findByText("This account doesn't hold any tokens or NFTs")
+}
+
+// @ts-ignore
+WithResources.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const { findByText } = canvas
+  await findByText('Tokens (fungible resources)')
+  await findByText('NFTs (nonfungible resources)')
+}
