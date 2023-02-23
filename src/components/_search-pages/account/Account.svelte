ca7d62441b0b17a92c9b3b13747d0b82d8862942
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-02-23 08:30:03 +0000

    refactor: make Account pure

--- src/components/_search-pages/account/Account.svelte
+++ src/components/_search-pages/account/Account.svelte
@@ -4,77 +4,69 @@
   import Card from '@components/_base/card/Card.svelte'
   import Text from '@components/_base/text/Text.svelte'
   import ResourceViewTitle from '@components/resource-view-title/ResourceViewTitle.svelte'
-  import { goto } from '$app/navigation'
-  import { query } from '@api/query'
   import Row from '@components/info-box/Row.svelte'
   import Textarea from '@components/_base/textarea/Textarea.svelte'
+  import type { getResources } from '@components/_navbar-pages/send-tokens/side-effects'
+  import { SkeletonLoader } from '@aleworm/svelte-skeleton-loader'
+  import type { EntityDetailsResponse } from '@radixdlt/babylon-gateway-api-sdk'
 
   export let address: string
+  export let resources: ReturnType<typeof getResources>
+  export let details: Promise<Pick<EntityDetailsResponse, 'metadata'>>
 
-  $: ({ send: getEntityDetails, response: entityDetailsResponse } =
-    query('getEntityDetails'))
-
-  $: ({
-    send: getResources,
-    response: resourcesResponse,
-    error
-  } = query('getResources'))
-
-  $: getEntityDetails(address)
-
-  $: getResources(address)
-
-  $: if ($error) goto('/not-found')
+  const values = Promise.all([resources, details])
 </script>
 
 <ResourceViewTitle title="Account" resourceAddress={address} />
 
 <Box>
-  {#if $resourcesResponse?.fungible.length === 0 && $resourcesResponse.nonFungible.length === 0}
-    This account doesn’t hold any tokens or NFTs
-  {:else}
-    {#if $resourcesResponse?.fungible.length}
+  {#await values}
+    <SkeletonLoader />
+  {:then [resources, details]}
+    {#if resources.fungible.length === 0 && resources.nonFungible.length === 0}
+      This account doesn't hold any tokens or NFTs
+    {:else}
+      {#if resources.fungible.length}
+        <Card>
+          <Text bold slot="header">Tokens (fungible resources)</Text>
+          <Box bgColor="surface" p="none" slot="body">
+            <InfoBox>
+              {#each resources.fungible as fungible}
+                <Row>
+                  <Text slot="left" bold underlined align="right">
+                    <a href="/resource/{fungible.address}">{fungible.key}</a>
+                  </Text>
+                  <Text slot="right">
+                    {fungible.value}
+                  </Text>
+                </Row>
+              {/each}
+            </InfoBox>
+          </Box>
+        </Card>
+      {/if}
+      {#if resources.nonFungible.length}
+        <Card>
+          <Text bold slot="header">NFTs (nonfungible resources)</Text>
+          <Box bgColor="surface" slot="body" p="none">
+            <InfoBox>
+              {#each resources.nonFungible as nft}
+                <Row>
+                  <Text slot="left" bold underlined>
+                    <a href="/nft/{nft.address}">{nft.key}</a>
+                  </Text>
+                  <div />
+                </Row>
+              {/each}
+            </InfoBox>
+          </Box>
+        </Card>
+      {/if}
       <Card>
-        <Text bold slot="header">Tokens (fungible resources)</Text>
-        <Box bgColor="surface" p="none" slot="body">
-          <InfoBox>
-            {#each $resourcesResponse.fungible as fungible}
-              <Row>
-                <Text slot="left" bold underlined align="right">
-                  <a href="/resource/{fungible.address}">{fungible.key}</a>
-                </Text>
-                <Text slot="right">
-                  {fungible.value}
-                </Text>
-              </Row>
-            {/each}
-          </InfoBox>
-        </Box>
-      </Card>
-    {/if}
-    {#if $resourcesResponse?.nonFungible.length}
-      <Card>
-        <Text bold slot="header">NFTs (nonfungible resources)</Text>
+        <Text bold slot="header">Metadata</Text>
         <Box bgColor="surface" slot="body" p="none">
           <InfoBox>
-            {#each $resourcesResponse.nonFungible as nft}
-              <Row>
-                <Text slot="left" bold underlined>
-                  <a href="/nft/{nft.address}">{nft.key}</a>
-                </Text>
-                <div />
-              </Row>
-            {/each}
-          </InfoBox>
-        </Box>
-      </Card>
-    {/if}
-    <Card>
-      <Text bold slot="header">Metadata</Text>
-      <Box bgColor="surface" slot="body" p="none">
-        <InfoBox>
-          {#if $entityDetailsResponse}
-            {#each $entityDetailsResponse.metadata.items as metadata}
+            {#each details.metadata.items as metadata}
               <Row>
                 <Text slot="left" bold align="right">{metadata.key}</Text>
                 <Textarea
@@ -88,9 +80,9 @@
                 />
               </Row>
             {/each}
-          {/if}
-        </InfoBox>
-      </Box>
-    </Card>
-  {/if}
+          </InfoBox>
+        </Box>
+      </Card>
+    {/if}
+  {/await}
 </Box>
--- src/routes/account/[account]/+page.svelte
+++ src/routes/account/[account]/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+  import { getEntityDetails } from '@api/gateway'
+  import { getResources } from '@components/_navbar-pages/send-tokens/side-effects'
   import Account from '@components/_search-pages/account/Account.svelte'
   import type { PageData } from './$types'
 
   export let data: PageData
+
+  $: details = getEntityDetails(data.accountAddress)
+
+  $: resources = getResources(data.accountAddress)
 </script>
 
-<Account address={data.accountAddress} />
+<Account address={data.accountAddress} {resources} {details} />
