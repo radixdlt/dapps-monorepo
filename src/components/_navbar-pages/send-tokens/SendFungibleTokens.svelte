3c0b810236f1fa8da83e835ca3ba4c1431dcde65
Dawid Sowa <dawid.sowa@rdx.works>
2023-03-01 13:55:08 +0100

    refactor: send fungible

---
+++ src/components/_navbar-pages/send-tokens/SendFungibleTokens.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Option } from '@components/_base/select/Select.svelte'
+  import SendFungible from './SendFungible.svelte'
+  import SendTokenForm from './SendTokens.svelte'
+
+  type OptionsType = Option<{ address: string }>
+
+  export let accounts: OptionsType[] | undefined = undefined
+</script>
+
+<SendTokenForm
+  {accounts}
+  let:resources
+  let:selectedFromAccount
+  let:selectedToAccount
+  let:setResourceSelected
+  let:setTransactionManifest
+>
+  <SendFungible
+    {selectedFromAccount}
+    {selectedToAccount}
+    resources={resources?.fungible || []}
+    {setTransactionManifest}
+    {setResourceSelected}
+  />
+</SendTokenForm>
--- src/routes/send-tokens/+page.svelte
+++ src/routes/send-tokens/+page.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
   import NavbarPage from '@components/_navbar-pages/navbar-page/NavbarPage.svelte'
-  import SendFungible from '@components/_navbar-pages/send-tokens/SendFungible.svelte'
-  import SendTokenForm from '@components/_navbar-pages/send-tokens/SendTokens.svelte'
+  import SendFungibleTokens from '@components/_navbar-pages/send-tokens/SendFungibleTokens.svelte'
 </script>
 
 <NavbarPage title="Send Tokens" let:accounts>
-  <SendTokenForm
-    {accounts}
-    let:resources
-    let:selectedFromAccount
-    let:selectedToAccount
-    let:setResourceSelected
-    let:setTransactionManifest
-  >
-    <SendFungible
-      {selectedFromAccount}
-      {selectedToAccount}
-      resources={resources?.fungible || []}
-      {setTransactionManifest}
-      {setResourceSelected}
-    />
-  </SendTokenForm>
+  <SendFungibleTokens {accounts} />
 </NavbarPage>
