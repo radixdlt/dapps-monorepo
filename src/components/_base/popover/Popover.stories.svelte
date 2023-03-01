d62ec0b86b43550255ec0975ab80902d81f5463b
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-03-01 10:10:06 +0000

    feat: implement popover

--- src/components/_base/popover/Popover.stories.svelte
+++ src/components/_base/popover/Popover.stories.svelte
@@ -1,13 +1,50 @@
 <script>
-  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
   import Popover from './Popover.svelte'
+
+  let show = false
 </script>
 
-<Meta title="Popover" />
+<Meta
+  title="Popover"
+  argTypes={{
+    placement: {
+      options: [
+        'right',
+        'right-start',
+        'right-end',
+        'left',
+        'left-start',
+        'left-end',
+        'top',
+        'top-start',
+        'top-end',
+        'bottom',
+        'bottom-start',
+        'bottom-end'
+      ],
+      control: { type: 'select' }
+    }
+  }}
+/>
 
-<Story name="Primary">
-  <Popover>
-    <button slot="button">Open panel</button>
-    <div slot="content">Panel</div>
-  </Popover>
-</Story>
+<Template let:args>
+  <div style:padding="50px">
+    <Popover type="error" placement={args.placement} bind:show>
+      <div
+        on:mouseenter={() => (show = true)}
+        on:mouseleave={() => (show = false)}
+      >
+        Hover me
+      </div>
+      <div slot="content">Hello</div>
+    </Popover>
+  </div>
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+    placement: 'right'
+  }}
+/>
