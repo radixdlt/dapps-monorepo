25e983c3fd3344f0b09cad492a571ece999eaa73
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-03-01 10:10:25 +0000

    feat: add url validation in app metadata

--- src/components/_navbar-pages/dapp-metadata/rows/Domain.svelte
+++ src/components/_navbar-pages/dapp-metadata/rows/Domain.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
   import Input from '@components/_base/input/Input.svelte'
   import Box from '@components/_base/box/Box.svelte'
+  import Popover from '@components/_base/popover/Popover.svelte'
+  import Text from '@components/_base/text/Text.svelte'
 
   export let faded: boolean
   export let domain: string
+
+  $: showError = !domain.startsWith('https://')
 </script>
 
 <Box wrapper cx={{ width: '$5xl' }}>
-  <Input
-    disabled={faded}
-    type="text"
-    bind:value={domain}
-    placeholder="Example: radixdlt.com"
-  />
+  <Popover type="error" placement="right" bind:show={showError}>
+    <Input
+      disabled={faded}
+      type="text"
+      bind:value={domain}
+      placeholder="Example: https://www.radixdlt.com"
+    />
+    <Text slot="content">url has to begin with https://</Text>
+  </Popover>
 </Box>
