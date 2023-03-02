1198e8e007d9f717e36600f59436980d7cdad5ff
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-03-02 09:01:35 +0000

    feat: redo loading spinner

--- src/components/_base/button/loading-spinner/LoadingSpinner.svelte
+++ src/components/_base/button/loading-spinner/LoadingSpinner.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { css, keyframes } from '@styles'
 
-  const rotate = keyframes({
+  const animation = keyframes({
     '0%': {
       transform: 'rotate(0deg)'
     },
@@ -10,36 +10,16 @@
     }
   })
 
-  const spinnerColor = css({
-    position: 'relative',
-    width: '$sm',
-    height: '$sm',
-    div: {
-      boxSizing: 'border-box',
-      display: 'block',
-      position: 'absolute',
-      width: '$sm',
-      height: '$sm',
-      border: '2px solid #fff',
-      borderRadius: 30,
-      animation: `${rotate} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite`,
-      borderColor: 'white transparent transparent transparent'
-    },
-    'div:nth-child(1)': {
-      animationDelay: '-0.45s'
-    },
-    'div:nth-child(2)': {
-      animationDelay: '-0.3s'
-    },
-    'div:nth-child(3)': {
-      animationDelay: '-0.15s'
-    }
-  })
+  const loader = css({
+    width: '100%',
+    aspectRatio: '1 / 1',
+    border: '3px solid #fff',
+    borderBottomColor: '$muted',
+    borderRadius: '50%',
+    display: 'inline-block',
+    boxSizing: 'border-box',
+    animation: `${animation} 1s linear infinite`
+  })()
 </script>
 
-<div class={spinnerColor()}>
-  <div />
-  <div />
-  <div />
-  <div />
-</div>
+<div class={loader} />
