8ccb872686cca5bce7f50cb5f31866d505083871
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-03-08 15:09:23 +0000

    feat: add CodeBox component

---
+++ src/components/code-box/CodeBox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let text: string
+</script>
+
+<textarea class="code-box" disabled value={text} />
+
+<style>
+  .code-box {
+    width: 100%;
+    height: 100%;
+    padding: var(--space-sm);
+    font-family: 'Hack';
+    background-color: var(--colors-background);
+    resize: none;
+    border-radius: var(--radii-md);
+  }
+</style>
