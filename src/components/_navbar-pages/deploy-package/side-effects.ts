864b459002eaa8f6b548d0e2873cfc2ccd608522
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-03-24 09:43:31 +0000

    feat: update nft tx manifest

--- src/components/_navbar-pages/deploy-package/side-effects.ts
+++ src/components/_navbar-pages/deploy-package/side-effects.ts
@@ -8,29 +8,26 @@ import { sendTransaction } from '@api/wallet'
 import { hash } from '@utils'
 
 export const getCreateBadgeManifest = (accountAddress: string) => `
-  CREATE_NON_FUNGIBLE_RESOURCE 
-      Enum("NonFungibleIdType::Integer") 
-      Map<String, String>(
-          "name", "My Package Owner Badge", 
-          "description", "This NFT was created by the Radix Dashboard as a simple badge to be used for default package control permissions. There is nothing special about it - swap it out, or create your own"
-      ) 
-      Map<Enum, Tuple>(
-          Enum("ResourceMethodAuthKey::Withdraw"), Tuple(Enum("AccessRule::AllowAll"), Enum("AccessRule::DenyAll")),
-          Enum("ResourceMethodAuthKey::Deposit"), Tuple(Enum("AccessRule::AllowAll"), Enum("AccessRule::DenyAll"))
-      )
-      Some(
-        Map<NonFungibleLocalId, Tuple>(
-            NonFungibleLocalId("#1#"), 
-            Tuple(
-                Tuple("Hello World", Decimal("12")),        
-                Tuple(12u8, 19u128)                         
-            )
-        )
+  CREATE_NON_FUNGIBLE_RESOURCE_WITH_INITIAL_SUPPLY
+    Enum("NonFungibleIdType::Integer")
+    Tuple(Tuple(Array<Enum>(), Array<Tuple>(), Array<Enum>()), Enum(0u8, 64u8), Array<String>())
+    Map<String, String>(
+        "name", "My Package Owner Badge",
+        "description", "This NFT was created by the Radix Dashboard as a simple badge to be used for default package control permissions. There is nothing special about it - swap it out, or create your own"
+    )
+    Map<Enum, Tuple>(
+        Enum("ResourceMethodAuthKey::Withdraw"), Tuple(Enum("AccessRule::AllowAll"), Enum("AccessRule::DenyAll")),
+        Enum("ResourceMethodAuthKey::Deposit"), Tuple(Enum("AccessRule::AllowAll"), Enum("AccessRule::DenyAll"))
+    )
+    Map<NonFungibleLocalId, Tuple>(
+        NonFungibleLocalId("#1#"),
+        Tuple(Tuple("Hello World", Decimal("12")))
     );
-  CALL_METHOD
-      ComponentAddress("${accountAddress}") 
-      "deposit_batch"
-      Expression("ENTIRE_WORKTOP");
+
+CALL_METHOD
+    Address("${accountAddress}") 
+    "deposit_batch"
+    Expression("ENTIRE_WORKTOP");
 `
 
 export const getDeployPackageManifest = (
