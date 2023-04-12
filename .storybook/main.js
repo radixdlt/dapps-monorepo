3e68e7e9cf57d8182baf35e7c5d4cf9bac4f8f52
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-04-12 13:47:56 +0100

    feat: storybook 7, vite 4 and sveltekit 1.15.4

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,44 +1,12 @@
-const { mergeConfig } = require('vite')
-
-const aliasWithSlash = (alias) =>
-  Object.entries(alias).reduce(
-    (obj, [key, value]) => ({
-      ...obj,
-      [key]: `/${value}`
-    }),
-    {}
-  )
-
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
-  ],
-  addons: [
-    'storybook-addon-mock',
-    '@storybook/addon-links',
-    '@storybook/addon-svelte-csf',
-    '@storybook/addon-essentials',
-    '@storybook/addon-actions',
-    '@storybook/addon-interactions',
-    '@storybook/addon-coverage'
-  ],
-  framework: '@storybook/svelte',
-  core: {
-    builder: '@storybook/builder-vite'
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+  addons: ['storybook-addon-mock', '@storybook/addon-links', '@storybook/addon-svelte-csf', '@storybook/addon-essentials', '@storybook/addon-actions', '@storybook/addon-interactions', '@storybook/addon-coverage', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/sveltekit',
+    options: {}
   },
   staticDirs: ['../static'],
-  async viteFinal(config, { configType }) {
-    // return the customized config
-    return mergeConfig(config, {
-      // customize the Vite config here
-      resolve: {
-        alias: {
-          ...aliasWithSlash((await import('../configs.js')).configs.alias),
-          '$env/static/public': '.storybook/__mocks__/env.ts',
-          '$app/navigation': '.storybook/__mocks__/navigation.ts'
-        }
-      }
-    })
+  docs: {
+    autodocs: true
   }
-}
+};
