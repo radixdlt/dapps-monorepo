814119b59d410cdfb26f3fa1a7befcc818c3c163
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2023-04-15 01:08:10 +0100

    fix: linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,15 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-    extraFileExtensions: [".svelte"], 
-  },
-  plugins: ['unused-imports'],
-  extends: ['plugin:svelte/recommended', 'plugin:storybook/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['unused-imports', 'svelte3', '@typescript-eslint'],
+  extends: ['plugin:storybook/recommended', 'prettier'],
   overrides: [
     {
       files: ["*.svelte"],
-      parser: "svelte-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-      },
+      processor: 'svelte3/svelte3'
     },
   ],
-  rules: {
-    semi: [0, 'never'],
-    'unused-imports/no-unused-imports': 'error',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    "svelte/html-quotes": [
-      "error",
-      {
-        "prefer": "double",
-        "dynamic": {
-          "quoted": false,
-          "avoidInvalidUnquotedInHTML": false
-        }
-      }
-    ],
-    "svelte/no-useless-mustaches": [
-      "error",
-      {
-        "ignoreIncludesComment": false,
-        "ignoreStringEscape": false
-      }
-    ]
-  },
+  settings: {
+    'svelte3/typescript': () => require('typescript')
+  }
 };
 
