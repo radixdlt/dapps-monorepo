609c1764bef369ab895f5037c3b7e56b5db4dbb2
Alex Stelea <alex.s@rdx.works>
2023-06-05 11:48:47 +0100

    feat: add address component

---
+++ src/components/_base/address/Address.stories.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Address from './Address.svelte'
+</script>
+
+<Meta title="Address" component={Address} />
+
+<Template>
+  <h1>Account</h1>
+  <Address
+    value={'account_tdx_c_1p9we7lz9az6fyarwzx2yxezx9ymfdtc6l4dsva4v36rqpek3pe'}
+  />
+</Template>
+
+<Story name="Primary" />
