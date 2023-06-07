55981baf14363b39776711cb804bc80a2138d7ae
Dawid Sowa <dawid.sowa@rdx.works>
2023-06-07 15:00:55 +0200

    fix: remove `IconNew` svelte warnings

--- src/components/_base/icon/IconNew.svelte
+++ src/components/_base/icon/IconNew.svelte
@@ -5,7 +5,7 @@
   export let size: 'small' | 'medium' | 'large' = 'medium'
 </script>
 
-<img class={size} on:click src={icons[type]} />
+<img class={size} src={icons[type]} alt="icon {type}" />
 
 <style>
   img {
