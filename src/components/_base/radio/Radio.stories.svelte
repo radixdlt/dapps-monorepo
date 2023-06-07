a2784a5bb0368c8cf51974f34a27a53a708b6349
Dawid Sowa <dawid.sowa@rdx.works>
2023-06-07 15:00:55 +0200

    feat: add account & radio components

---
+++ src/components/_base/radio/Radio.stories.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Meta, Story } from '@storybook/addon-svelte-csf'
+    import Radio from './Radio.svelte'
+</script>
+
+<Meta title="Radio" />
+
+<Story name="Selected">
+  <Radio selected />
+</Story>
+
+<Story name="Not Selected">
+  <Radio />
+</Story>
