e0f400c3cbd0f77ce18dfef458dc09b7ed438c29
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-06-09 17:58:37 +0100

    feat: add amount input  component

---
+++ src/components/_base/input/AmountInput.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { number } from '@directives/format-input'
+  import InputNew from './InputNew.svelte'
+  import type { ComponentProps } from 'svelte'
+  import numbro from 'numbro'
+
+  export let displayedValue: string = ''
+  export let inputParams:
+    | Omit<ComponentProps<InputNew>, 'formatter' | 'width' | 'value'>
+    | undefined = undefined
+
+  export let value = 0
+
+  $: value = numbro.unformat(displayedValue)
+</script>
+
+<div class="input-box">
+  <InputNew
+    bind:displayedValue
+    {...inputParams}
+    format={number()}
+    --font-size="var(--text-3xl)"
+    --font-weight="var(--font-weight-bold-2)"
+    --text-align="right"
+  />
+</div>
+
+<style lang="scss">
+  @use './shared.scss';
+</style>
