7b7a75b539ca574263feec1add1e364a0a0018ae
Alex Stelea <xstelea@gmail.com>
2023-06-13 12:58:36 +0100

    build: migrate to monorepo (#446)
    * build: migrate to monorepo
    
    * ci: setup build and deployment
    
    * ci: update pre-compile command
    
    * build: copy node modules from root
    
    ---------
    
    Co-authored-by: Alex Stelea <alex.s@rdx.works>

---
+++ apps/dashboard/src/components/_base/text/Text.svelte
@@ -0,0 +1,314 @@
+<script lang="ts">
+  import type { CSS } from '@stitches/core'
+
+  import { config, css } from '../../../styles'
+
+  const boldClass = css({
+    fontWeight: 'bold'
+  })()
+
+  const underlinedClass = css({
+    textDecoration: 'underline'
+  })
+
+  const variants = {
+    nowrap: {
+      true: {
+        whiteSpace: 'nowrap'
+      }
+    },
+    align: {
+      center: {
+        textAlign: 'center'
+      },
+      left: {
+        textAlign: 'left'
+      },
+      right: {
+        textAlign: 'right'
+      }
+    },
+    inline: {
+      true: {
+        display: 'inline-flex'
+      }
+    },
+    cellAlign: {
+      right: {
+        float: 'right'
+      }
+    },
+    pointer: {
+      true: {
+        cursor: 'pointer'
+      }
+    },
+    mb: {
+      xsmall: {
+        mb: '$xs'
+      },
+      small: {
+        mb: '$sm'
+      },
+      medium: {
+        mb: '$md'
+      },
+      large: {
+        mb: '$lg'
+      },
+      none: {
+        mb: '$0'
+      }
+    },
+    items: {
+      center: {
+        display: 'flex',
+        alignItems: 'center'
+      }
+    },
+    ml: {
+      auto: {
+        ml: '$auto'
+      },
+      small: {
+        ml: '$sm'
+      }
+    },
+    mt: {
+      xsmall: {
+        mt: '$xs'
+      },
+      small: {
+        mt: '$sm'
+      },
+      medium: {
+        mt: '$md'
+      },
+      large: {
+        mt: '$lg'
+      },
+      none: {
+        mt: '$0'
+      }
+    },
+    m: {
+      xsmall: {
+        m: '$xs'
+      },
+      small: {
+        m: '$sm'
+      },
+      medium: {
+        m: '$md'
+      },
+      large: {
+        m: '$lg'
+      },
+      none: {
+        m: '$0'
+      }
+    },
+    p: {
+      xsmall: {
+        p: '$xs'
+      },
+      small: {
+        p: '$sm'
+      },
+      medium: {
+        p: '$md'
+      },
+      large: {
+        p: '$lg'
+      },
+      none: {
+        p: '$0'
+      }
+    },
+    mx: {
+      xsmall: {
+        mx: '$xs'
+      },
+      small: {
+        mx: '$sm'
+      },
+      medium: {
+        mx: '$md'
+      },
+      large: {
+        mx: '$lg'
+      },
+      none: {
+        mx: '$0'
+      }
+    },
+    my: {
+      xsmall: {
+        my: '$xs'
+      },
+      small: {
+        my: '$sm'
+      },
+      medium: {
+        my: '$md'
+      },
+      large: {
+        my: '$lg'
+      },
+      none: {
+        my: '$0'
+      }
+    },
+    py: {
+      xsmall: {
+        py: '$xs'
+      },
+      small: {
+        py: '$sm'
+      },
+      medium: {
+        py: '$md'
+      },
+      large: {
+        py: '$lg'
+      },
+      none: {
+        py: '$0'
+      }
+    },
+    px: {
+      xsmall: {
+        px: '$xs'
+      },
+      small: {
+        px: '$sm'
+      },
+      medium: {
+        px: '$md'
+      },
+      large: {
+        px: '$lg'
+      },
+      none: {
+        px: '$0'
+      }
+    },
+    color: {
+      primary: {
+        color: '$primary'
+      },
+      secondary: {
+        color: '$secondary'
+      },
+      tertiary: {
+        color: '$tertiary'
+      },
+      connect: {
+        color: '$connectButtonPopover'
+      },
+      link: {
+        color: '$highlightedText'
+      },
+      grey: {
+        color: '$grey'
+      },
+      error: {
+        color: '$error'
+      },
+      success: { color: '$success' }
+    },
+    slightlyMuted: {
+      true: {
+        color: '$slightlyMuted'
+      }
+    },
+    muted: {
+      true: {
+        color: '$muted'
+      }
+    },
+    size: {
+      xsmall: {
+        fontSize: '$xs'
+      },
+      small: {
+        fontSize: '$sm'
+      },
+      medium: {
+        fontSize: '$md'
+      },
+      large: {
+        fontSize: '$lg'
+      },
+      xlarge: {
+        fontSize: '$xl'
+      },
+      xxlarge: {
+        fontSize: '$2xl'
+      }
+    }
+  } as const
+
+  export let cx: CSS<typeof config> = {}
+  export let full: true | false = false
+  export let pointer: true | false = false
+  export let bold: true | false = false
+  export let underlined: true | false = false
+  export let p: keyof (typeof variants)['p'] | undefined = undefined
+  export let m: keyof (typeof variants)['m'] | undefined = undefined
+  export let py: keyof (typeof variants)['py'] | undefined = undefined
+  export let px: keyof (typeof variants)['px'] | undefined = undefined
+  export let my: keyof (typeof variants)['my'] | undefined = undefined
+  export let mx: keyof (typeof variants)['mx'] | undefined = undefined
+  export let mt: keyof (typeof variants)['mt'] | undefined = undefined
+  export let ml: keyof (typeof variants)['ml'] | undefined = undefined
+  export let items: keyof (typeof variants)['items'] | undefined = undefined
+  export let mb: keyof (typeof variants)['mb'] | undefined = undefined
+  export let color: keyof (typeof variants)['color'] | undefined = undefined
+  export let size: keyof (typeof variants)['size'] | undefined = undefined
+  export let muted: true | false = false
+  export let slightlyMuted: true | false = false
+  export let cellAlign: keyof (typeof variants)['cellAlign'] | undefined =
+    undefined
+  export let inline: true | false = false
+  export let align: keyof (typeof variants)['align'] | undefined = undefined
+  export let nowrap: boolean = false
+
+  const text = css({
+    variants,
+    ...cx
+  })
+
+  $: textClass = text({
+    full,
+    pointer,
+    p,
+    px,
+    py,
+    m,
+    mt,
+    ml,
+    items,
+    mb,
+    mx,
+    my,
+    color,
+    size,
+    muted,
+    cellAlign,
+    slightlyMuted,
+    inline,
+    align,
+    nowrap
+  })
+</script>
+
+<!-- eslint-disable-next-line -->
+<div
+  on:click
+  class={`${textClass} ${bold ? boldClass : ''} ${
+    underlined ? underlinedClass : ''
+  }`}
+>
+  <slot />
+</div>
---
+++ apps/dashboard/src/components/_navbar-pages/staking/filter-details/manual-filter-card/ManualFilterCard.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import PercentageInput from '@components/_base/input/PercentageInput.svelte'
+  import SimplePicker from '@components/_base/picker/simple-picker/SimplePicker.svelte'
+
+  type T = $$Generic
+
+  export let options: {
+    label: string
+    value: T
+  }[]
+  export let selected: (typeof options)[number]
+  export let percentage = '0'
+</script>
+
+<div id="manual-filter-card">
+  <div id="value-card">
+    <div class="row">
+      <div id="text">MIN UPTIME (%)</div>
+      <SimplePicker {options} bind:selected />
+    </div>
+    <PercentageInput bind:value={percentage} />
+  </div>
+</div>
+
+<style lang="scss">
+  @use '../../../../../mixins.scss';
+  @use '../shared.scss';
+
+  #manual-filter-card {
+    @include mixins.card();
+    padding: var(--spacing-2xl);
+    width: 100%;
+  }
+
+  #value-card {
+    @include shared.value-card();
+
+    .row {
+      display: flex;
+      gap: var(--spacing-md);
+      align-items: center;
+
+      #text {
+        font-size: var(--card-text-size);
+        color: var(--card-text-color);
+      }
+    }
+  }
+</style>
