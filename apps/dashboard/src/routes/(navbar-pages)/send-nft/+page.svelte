e6c6356870bc9b6e02838ec58a2bfc2fb5789eb3
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-06-15 08:48:01 +0100

    fix: imports

--- apps/dashboard/src/routes/(navbar-pages)/send-nft/+page.svelte
+++ apps/dashboard/src/routes/(navbar-pages)/send-nft/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import NavbarPage from '../../../pages/_navbar-pages/NavbarPage.svelte'
-  import SendNonFungibleTokens from '../../../pages/_navbar-pages/send-tokens/SendNonFungibleTokens.svelte'
+  import NavbarPage from '../../../pages/navbar-pages/NavbarPage.svelte'
+  import SendNonFungibleTokens from '../../../pages/navbar-pages/send-tokens/SendNonFungibleTokens.svelte'
 </script>
 
 <NavbarPage title="Send NFTs" let:accounts>
--- apps/dashboard/src/routes/(search-pages)/account/[account]/+page.svelte
+++ apps/dashboard/src/routes/(search-pages)/account/[account]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { getAccountData } from '@api/utils/resources'
-  import Account from '@pages/_search-pages/account/Account.svelte'
-  import SearchPage from '@pages/_search-pages/SearchPage.svelte'
+  import Account from '@pages/search-pages/account/Account.svelte'
+  import SearchPage from '@pages/search-pages/SearchPage.svelte'
   import type { PageData } from './$types'
 
   export let data: PageData
