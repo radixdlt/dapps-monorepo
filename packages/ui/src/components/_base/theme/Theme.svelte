19fffda2060650f5791224b8b83683bbff974f6d
Alex Stelea <alex.s@rdx.works>
2023-06-16 10:43:16 +0100

    style: use theme styles

--- packages/ui/src/components/_base/theme/Theme.svelte
+++ packages/ui/src/components/_base/theme/Theme.svelte
@@ -14,6 +14,12 @@
     --subtext-color: var(--color-grey-2);
     --error-dark: var(--color-radix-error-red-1);
     --error-light: var(--color-radix-error-red-2);
+    --border: var(--color-grey-3);
+
+    /* Pills menu */
+    --pills-menu-item-active-background: var(--color-grey-1);
+    --pills-menu-item-active-text: var(--color-grey-5);
+    --pills-menu-item-hover-background: var(--color-grey-4);
   }
   .dark {
     --background: var(--color-tertiary-dark);
