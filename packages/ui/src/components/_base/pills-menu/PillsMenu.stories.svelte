e2ab9e3256b4d66387db04a6b0996738dde2d5a0
Alex Stelea <alex.s@rdx.works>
2023-06-16 10:04:22 +0100

    feat: add pills menu

---
+++ packages/ui/src/components/_base/pills-menu/PillsMenu.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import PillsMenu from './PillsMenu.svelte'
+</script>
+
+<Meta title="Base components / Pills menu" />
+
+<Template>
+  <PillsMenu
+    active="token"
+    onClick={(id) => console.log(`clicked: ${id}`)}
+    items={[
+      [
+        { id: 'token', label: 'Tokens' },
+        { id: 'ntfs', label: 'NFTs' },
+        { id: 'pool-units', label: 'Pool Units' }
+      ],
+      [
+        { id: 'recent-transactions', label: 'Recent Transactions' },
+        { id: 'Metadata', label: 'Metadata' }
+      ]
+    ]}
+  />
+</Template>
+
+<Story name="Primary" />
