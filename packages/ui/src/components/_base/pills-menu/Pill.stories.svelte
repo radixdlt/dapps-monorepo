0f7bd889c6768d1dc8ef3580354549abbf47bf75
Alex Stelea <alex.s@rdx.works>
2023-06-16 10:04:22 +0100

    feat: add pills menu

---
+++ packages/ui/src/components/_base/pills-menu/Pill.stories.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Pill from './Pill.svelte'
+</script>
+
+<Meta title="Base components / Pills menu / Pill" />
+
+<Template>
+  <Pill active>Tokens</Pill>
+  <Pill>Nfts</Pill>
+</Template>
+
+<Story name="Primary" />
---
+++ packages/ui/src/components/_base/pills-menu/Pill.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let active = false
+</script>
+
+<button style={$$props.style} class="pill" class:active on:click
+  ><slot /></button
+>
+
+<style lang="scss">
+  .pill {
+    &:hover {
+      background: var(--color-grey-4);
+    }
+    &.active {
+      background: var(--color-grey-1);
+      color: var(--color-grey-5);
+    }
+    white-space: nowrap;
+    transition: background-color 0.1s ease-in-out;
+    border-radius: 50px;
+    padding: 0.6rem 1rem;
+    font-weight: var(--font-weight-bold-2);
+    user-select: none;
+  }
+</style>
