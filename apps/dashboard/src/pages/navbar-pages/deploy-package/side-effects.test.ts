7f8862e766859a98d62a5d6dbbda4b68e821f6f7
Dawid Sowa <dawid.sowa@rdx.works>
2023-06-19 14:48:33 +0200

    feat: bump deps to `birch`, adjust manifests

--- apps/dashboard/src/pages/navbar-pages/deploy-package/side-effects.test.ts
+++ apps/dashboard/src/pages/navbar-pages/deploy-package/side-effects.test.ts
@@ -1,5 +1,7 @@
 import {
   InstructionList,
+  RadixEngineToolkit,
+  SborValue,
   TransactionManifest
 } from '@radixdlt/radix-engine-toolkit'
 import {
@@ -26,18 +28,28 @@ describe('Deploy Package Transaction Manifests', () => {
   })
 
   it('should create deploy package manifest', async () => {
-    const faucet = readFileSync(join(__dirname, 'faucet.wasm')).toString('hex')
+    const faucetSchema = readFileSync(
+      join(__dirname, 'faucet.schema')
+    ).toString('hex')
+    const faucetWasm = readFileSync(join(__dirname, 'faucet.wasm')).toString(
+      'hex'
+    )
+
+    const sborDecodedSchema = (await RadixEngineToolkit.sborDecode(
+      faucetSchema,
+      34
+    )) as SborValue.ManifestSbor
 
     const stringManifest = getDeployPackageManifest(
-      faucet,
-      `Tuple(Map<String, Tuple>("Faucet", Tuple(Enum(0u8), Tuple(Array<Enum>(Enum(14u8, Array<Enum>(Enum(0u8, 147u8), Enum(0u8, 148u8))), Enum(14u8, Array<Enum>(Enum(1u8, 2u64), Enum(0u8, 145u8))), Enum(13u8, Enum(0u8, 7u8)), Enum(14u8, Array<Enum>()), Enum(14u8, Array<Enum>(Enum(0u8, 160u8)))), Array<Tuple>(Tuple(Enum(1u8, "Faucet"), Enum(1u8, Enum(0u8, Array<String>("vault", "transactions")))), Tuple(Enum(1u8, "Faucet_new_Input"), Enum(1u8, Enum(0u8, Array<String>("arg0", "arg1")))), Tuple(Enum(1u8, "Bytes"), Enum(0u8)), Tuple(Enum(1u8, "Faucet_free_Input"), Enum(1u8, Enum(0u8, Array<String>()))), Tuple(Enum(1u8, "Faucet_lock_fee_Input"), Enum(1u8, Enum(0u8, Array<String>("arg0"))))), Array<Enum>(Enum(0u8), Enum(0u8), Enum(12u8, Tuple(Enum(1u8, 30u32), Enum(1u8, 30u32))), Enum(0u8), Enum(0u8))), Array<Enum>(Enum(1u8, 0u64)), Array<Enum>(), Map<String, Tuple>("free", Tuple(Enum(1u8, Enum(1u8)), Enum(1u8, 3u64), Enum(0u8, 145u8), "Faucet_free"), "lock_fee", Tuple(Enum(1u8, Enum(1u8)), Enum(1u8, 4u64), Enum(0u8, 66u8), "Faucet_lock_fee"), "new", Tuple(Enum(0u8), Enum(1u8, 1u64), Enum(0u8, 132u8), "Faucet_new")), Map<U8, Tuple>(), Map<String, Enum>())))`,
+      faucetWasm,
+      sborDecodedSchema.manifestString,
       'resource_tdx_22_1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxmaesev',
       '#65#'
     )
 
     let manifest = new TransactionManifest(
       new InstructionList.StringInstructions(stringManifest),
-      [faucet]
+      [faucetWasm]
     )
 
     await expect(
--- apps/dashboard/src/pages/navbar-pages/deploy-package/side-effects.ts
+++ apps/dashboard/src/pages/navbar-pages/deploy-package/side-effects.ts
@@ -3,25 +3,25 @@ import { hash } from '@utils'
 
 export const getCreateBadgeManifest = (accountAddress: string) => `
   CREATE_NON_FUNGIBLE_RESOURCE_WITH_INITIAL_SUPPLY
-    Enum("NonFungibleIdType::Integer")
-    Tuple(Tuple(Array<Enum>(), Array<Tuple>(), Array<Enum>()), Enum(0u8, 64u8), Array<String>())
-    Map<String, String>(
-        "name", "My Package Owner Badge",
-        "description", "This NFT was created by the Radix Dashboard as a simple badge to be used for default package control permissions. There is nothing special about it - swap it out, or create your own"
+    Enum<NonFungibleIdType::Integer>()
+    Tuple(Tuple(Array<Enum>(), Array<Tuple>(), Array<Enum>()), Enum<0u8>(64u8), Array<String>())
+    Map<String, Enum>(
+        "name" => Enum<Metadata::String>("My Package Owner Badge"),                                     
+        "description" => Enum<Metadata::String>("This NFT was created by the Radix Sandbox dApp as a simple badge to be used for default package control permissions.")   
     )
     Map<Enum, Tuple>(
-        Enum("ResourceMethodAuthKey::Withdraw"), Tuple(Enum("AccessRule::AllowAll"), Enum("AccessRule::DenyAll")),
-        Enum("ResourceMethodAuthKey::Deposit"), Tuple(Enum("AccessRule::AllowAll"), Enum("AccessRule::DenyAll"))
+        Enum<ResourceMethodAuthKey::Withdraw>() => Tuple(Enum<AccessRule::AllowAll>(), Enum<AccessRule::DenyAll>()),
+        Enum<ResourceMethodAuthKey::Deposit>() => Tuple(Enum<AccessRule::AllowAll>(), Enum<AccessRule::DenyAll>())
     )
     Map<NonFungibleLocalId, Tuple>(
-        NonFungibleLocalId("#1#"),
+        NonFungibleLocalId("#1#") => 
         Tuple(Tuple("Hello World", Decimal("12")))
     );
 
 CALL_METHOD
     Address("${accountAddress}") 
     "deposit_batch"
-    Expression("ENTIRE_WORKTOP");
+    Expression("ENTIRE_WORKTOP"); 
 `
 
 export const getDeployPackageManifest = (
@@ -33,176 +33,13 @@ export const getDeployPackageManifest = (
   const wasmHash: string = hash(wasm).toString('hex')
 
   return `
-   PUBLISH_PACKAGE_ADVANCED
-    Blob("${wasmHash}") 
-    ${schema}
-    Map<String, Tuple>()       # Royalty Configuration
-    Map<String, String>()      # Metadata 
-    Tuple(                     # Access Rules Config Struct
-        Map<Tuple, Enum>(),     # Direct Access Method auth Field
-        Map<Tuple, Enum>(       # Method auth Field
-            Tuple(
-                Enum("TypedModuleId::ObjectState"),
-                "set_royalty_config"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule", 
-                Enum(
-                    "AccessRule::Protected", 
-                    Enum(
-                        "AccessRuleNode::ProofRule", 
-                        Enum(
-                            "ProofRule::Require", 
-                            Enum(
-                                "SoftResourceOrNonFungible::StaticNonFungible", 
-                                NonFungibleGlobalId("${nftAddress}:${nftId}")
-                            )
-                        )
-                    )
-                )
-            ),
-            Tuple(
-                Enum("TypedModuleId::ObjectState"),
-                "claim_royalty"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule", 
-                Enum(
-                    "AccessRule::Protected", 
-                    Enum(
-                        "AccessRuleNode::ProofRule", 
-                        Enum(
-                            "ProofRule::Require", 
-                            Enum(
-                                "SoftResourceOrNonFungible::StaticNonFungible", 
-                                NonFungibleGlobalId("${nftAddress}:${nftId}")
-                            )
-                        )
-                    )
-                )
-            ),
-            Tuple(
-                Enum("TypedModuleId::Metadata"),
-                "set"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule", 
-                Enum(
-                    "AccessRule::Protected", 
-                    Enum(
-                        "AccessRuleNode::ProofRule", 
-                        Enum(
-                            "ProofRule::Require", 
-                            Enum(
-                                "SoftResourceOrNonFungible::StaticNonFungible", 
-                                NonFungibleGlobalId("${nftAddress}:${nftId}")
-                            )
-                        )
-                    )
-                )
-            ),
-            Tuple(
-                Enum("TypedModuleId::Metadata"),
-                "get"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule", 
-                Enum("AccessRule::AllowAll")
-            )
-        ), 
-        Map<String, Enum>(),     # Grouped Auth Field
-        Enum(
-            "AccessRuleEntry::AccessRule",
-            Enum("AccessRule::DenyAll")         # Default Auth Field
-        ),
-        Map<Tuple, Enum>(         # Method Auth Mutability Field
-            Tuple(
-                Enum("TypedModuleId::ObjectState"),
-                "set_royalty_config"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule",
-                Enum(
-                    "AccessRule::Protected",
-                    Enum(
-                        "AccessRuleNode::ProofRule",
-                        Enum(
-                            "ProofRule::Require",
-                            Enum(
-                                "SoftResourceOrNonFungible::StaticNonFungible",
-                                NonFungibleGlobalId("${nftAddress}:${nftId}")
-                            )
-                        )
-                    )
-                )
-            ),
-            Tuple(
-                Enum("TypedModuleId::ObjectState"),
-                "claim_royalty"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule",
-                Enum(
-                    "AccessRule::Protected",
-                    Enum(
-                        "AccessRuleNode::ProofRule",
-                        Enum(
-                            "ProofRule::Require",
-                            Enum(
-                                "SoftResourceOrNonFungible::StaticNonFungible",
-                                NonFungibleGlobalId("${nftAddress}:${nftId}")
-                            )
-                        )
-                    )
-                )
-            ),
-            Tuple(
-                Enum("TypedModuleId::Metadata"),
-                "set"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule",
-                Enum(
-                    "AccessRule::Protected",
-                    Enum(
-                        "AccessRuleNode::ProofRule",
-                        Enum(
-                            "ProofRule::Require",
-                            Enum(
-                                "SoftResourceOrNonFungible::StaticNonFungible",
-                                NonFungibleGlobalId("${nftAddress}:${nftId}")
-                            )
-                        )
-                    )
-                )
-            ),
-            Tuple(
-                Enum("TypedModuleId::Metadata"),
-                "get"
-            ),
-            Enum(
-                "AccessRuleEntry::AccessRule",
-                Enum(
-                    "AccessRule::Protected",
-                    Enum(
-                        "AccessRuleNode::ProofRule",
-                        Enum(
-                            "ProofRule::Require",
-                            Enum(
-                                "SoftResourceOrNonFungible::StaticNonFungible",
-                                NonFungibleGlobalId("${nftAddress}:${nftId}")
-                            )
-                        )
-                    )
-                )
-            )
-        ), 
-        Map<String, Enum>(),     # Group Auth Mutability Field
-        Enum(
-            "AccessRuleEntry::AccessRule",
-            Enum("AccessRule::DenyAll")          # Default Auth Mutability Field
-        )
-    );
+      PUBLISH_PACKAGE_ADVANCED
+        None
+        Blob("${wasmHash}") 
+        ${schema}
+        Map<String, Tuple>()      
+        Map<String, Enum>()  
+        Map<Enum, Tuple>();    
       `
 }
 
