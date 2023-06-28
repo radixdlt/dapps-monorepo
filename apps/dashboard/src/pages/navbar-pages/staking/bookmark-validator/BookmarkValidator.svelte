4a8612a8f7de82ffa25f15a302330920b7643839
Dawid Sowa <dawid.sowa@rdx.works>
2023-06-29 00:07:31 +0200

    feat: create BookmarkValidator component

---
+++ apps/dashboard/src/pages/navbar-pages/staking/bookmark-validator/BookmarkValidator.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import type { Validator } from '../Validators.svelte'
+  import { connected } from '@stores'
+  import { SkeletonLoader } from '@aleworm/svelte-skeleton-loader'
+  import { bookmarkedValidatorsStore } from '../../../../stores'
+  import { setFavoriteValidator } from '../../../../server/validators/validators-api'
+  import BookmarkEmptyIcon from '@icons/bookmark-empty.svg'
+  import BookmarkFilledIcon from '@icons/bookmark-filled.svg'
+  import IconNew from '@components/_base/icon/IconNew.svelte'
+
+  export let validator: Promise<Validator> | Validator
+  export let withText = false
+
+  $: promise = Promise.resolve(validator)
+</script>
+
+{#if $connected}
+  {#await promise}
+    <SkeletonLoader />
+  {:then validator}
+    <button
+      class="bookmarked"
+      on:click={() => {
+        $bookmarkedValidatorsStore[validator.address] =
+          !$bookmarkedValidatorsStore[validator.address]
+        bookmarkedValidatorsStore.set($bookmarkedValidatorsStore)
+        setFavoriteValidator(
+          validator.address,
+          $bookmarkedValidatorsStore[validator.address]
+        ).mapErr(() => {
+          $bookmarkedValidatorsStore[validator.address] =
+            !$bookmarkedValidatorsStore[validator.address]
+          bookmarkedValidatorsStore.set($bookmarkedValidatorsStore)
+        })
+      }}
+    >
+      <IconNew
+        icon={$bookmarkedValidatorsStore[validator.address]
+          ? BookmarkFilledIcon
+          : BookmarkEmptyIcon}
+        size="medium"
+      />
+      {#if withText}<span>Bookmarked</span>{/if}
+      
+    </button>
+  {/await}
+{/if}
+
+<style lang="scss">
+  .bookmarked {
+    display: flex;
+    align-items: center;
+    gap: var(--spacing-sm);
+    font-weight: var(--font-weight-bold-2);
+  }
+</style>
