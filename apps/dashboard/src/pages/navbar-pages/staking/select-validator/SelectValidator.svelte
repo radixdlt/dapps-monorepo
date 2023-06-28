a4a378b024409b7e00630e50192ae6b0b6c2477f
Dawid Sowa <dawid.sowa@rdx.works>
2023-06-29 00:07:44 +0200

    feat: create SelectValidator component

---
+++ apps/dashboard/src/pages/navbar-pages/staking/select-validator/SelectValidator.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Checkbox from '@components/_base/checkbox/Checkbox.svelte'
+  import { selectedValidators, type Validator } from '../Validators.svelte'
+
+  import { connected } from '@stores'
+  export let text: string
+  export let validator: Promise<Validator>
+  
+</script>
+
+{#if $connected}
+  {#await validator then validator}
+    <Checkbox
+      bind:checked={$selectedValidators[validator.address]}
+      on:checked={() => {
+        $selectedValidators = $selectedValidators
+      }}
+      on:unchecked={() => {
+        $selectedValidators = $selectedValidators
+      }}
+      --label-color="var(--color-grey-2)"
+    >
+      {text}
+    </Checkbox>
+  {/await}
+{/if}
