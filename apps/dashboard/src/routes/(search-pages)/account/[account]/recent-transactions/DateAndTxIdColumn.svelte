54581534fe3a450071f465e298aefb5730b5ae18
Dawid Sowa <dawid.sowa@rdx.works>
2023-07-04 16:08:07 +0200

    feat: add date and id column

---
+++ apps/dashboard/src/routes/(search-pages)/account/[account]/recent-transactions/DateAndTxIdColumn.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { shortenAddress } from '@utils'
+  import Link from '@components/_base/link/Link.svelte'
+
+  export let id: string
+  export let date: string
+
+  $: [, day, month, , time] = new Date(date).toUTCString().split(' ')
+  $: [hours, minutes] = time.split(':')
+</script>
+
+<div class="wrapper">
+  <span class="bolder subtext">
+    ID: <Link text={shortenAddress(id)} url={`/transaction/${id}`} />
+  </span>
+  <span class="date subtext">{day} {month} • {hours}:{minutes}</span>
+</div>
+
+<style lang="scss">
+  .bolder {
+    font-weight: var(--font-weight-bold-2);
+  }
+  .subtext {
+    color: var(--theme-subtext);
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
