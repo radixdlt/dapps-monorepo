7ce73c8517f0bd6ca087f08354b0f644a8b3f693
Dawid Sowa <dawid.sowa@rdx.works>
2023-07-05 17:27:34 +0200

    feat: implement `PaginatedTable`

--- apps/dashboard/src/routes/(search-pages)/account/[account]/recent-transactions/+page.svelte
+++ apps/dashboard/src/routes/(search-pages)/account/[account]/recent-transactions/+page.svelte
@@ -1,37 +1,14 @@
 <script lang="ts">
-  import { SkeletonLoader } from '@aleworm/svelte-skeleton-loader'
   import type { PageData } from './$types'
-  import Table from '@components/_base/table/Table.svelte'
-  import InfiniteScroll from '@components/infinite-scroll/InfiniteScroll.svelte'
+  import PaginatedTable from '@components/_base/table/PaginatedTable.svelte'
   import type { TableConfig } from '@components/_base/table/types'
   import DateAndTxIdColumn from './DateAndTxIdColumn.svelte'
   import { getRecentTransactions } from '@api/gateway'
 
   export let data: PageData
 
-  let entries: any[] = []
-  let isLoadingData = true
-  let loadingCursor: string | null | undefined = null
-  let nextCursor: string | null | undefined = null
-
-  data.promises.transactionList.then((data) => {
-    isLoadingData = false
-    nextCursor = data.next_cursor
-    entries = [...data.items]
-  })
-
-  const fetchMore = () => {
-    if (nextCursor && loadingCursor !== nextCursor) {
-      isLoadingData = true
-      loadingCursor = nextCursor
-      getRecentTransactions(data.account, nextCursor).then((data) => {
-        isLoadingData = false
-        loadingCursor = null
-        nextCursor = data.next_cursor
-        entries = [...entries, ...data.items]
-      })
-    }
-  }
+  const queryFunction = (cursor?: string) =>
+    getRecentTransactions(data.account, cursor)
 
   const config: TableConfig = {
     columns: [
@@ -57,22 +34,4 @@
   }
 </script>
 
-<Table {config} {entries} />
-
-{#if isLoadingData}
-  <div class="loading-wrapper">
-    <SkeletonLoader />
-  </div>
-{/if}
-
-<InfiniteScroll
-  on:thresholdReached={() => {
-    fetchMore()
-  }}
-/>
-
-<style lang="scss">
-  .loading-wrapper {
-    margin-bottom: var(--space-lg);
-  }
-</style>
+<PaginatedTable {config} {queryFunction} />
---
+++ packages/ui/src/components/_base/table/PaginatedTable.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { SkeletonLoader } from '@aleworm/svelte-skeleton-loader'
+  import Table from './Table.svelte'
+  import type { TableConfig, TablePage } from './types'
+  import InfiniteScroll from '@components/infinite-scroll/InfiniteScroll.svelte'
+  import Pagination from './Pagination.svelte'
+
+  type T = $$Generic
+
+  export let config: TableConfig
+  export let queryFunction: (cursor?: string) => Promise<TablePage<T>>
+
+  export let mode: 'infiniteScroll' | 'paginated' = 'infiniteScroll'
+
+  let entries: T[] = []
+
+  let nextCursor: string | undefined
+  let previousCursor: string | undefined
+  let isLoadingCursor: string | undefined = 'initial'
+
+  const updateCursors = (data: TablePage<T>) => {
+    nextCursor = data.next_cursor ?? undefined
+    previousCursor = data.previous_cursor ?? undefined
+  }
+
+  const updateEntries = (data: TablePage<T>) => {
+    updateCursors(data)
+    entries = [...entries, ...data.items]
+  }
+
+  const replaceEntries = (data: TablePage<T>) => {
+    updateCursors(data)
+    entries = data.items
+  }
+
+  onMount(() => {
+    queryFunction().then((data) => {
+      isLoadingCursor = undefined
+      updateEntries(data)
+    })
+  })
+
+  const fetchMore = () => {
+    if (nextCursor === isLoadingCursor || !nextCursor) {
+      return
+    }
+    isLoadingCursor = nextCursor
+    queryFunction(nextCursor).then((data) => {
+      isLoadingCursor = undefined
+      updateEntries(data)
+    })
+  }
+
+  const nextPage = () => {
+    isLoadingCursor = nextCursor
+    queryFunction(nextCursor).then((data) => {
+      isLoadingCursor = undefined
+      replaceEntries(data)
+    })
+  }
+
+  const previousPage = () => {
+    isLoadingCursor = previousCursor
+    queryFunction(previousCursor).then((data) => {
+      isLoadingCursor = undefined
+      replaceEntries(data)
+    })
+  }
+</script>
+
+<Table {config} {entries} />
+
+{#if isLoadingCursor && mode === 'infiniteScroll'}
+  <div class="bottom-space">
+    <SkeletonLoader />
+  </div>
+{/if}
+
+{#if mode === 'infiniteScroll'}
+  <InfiniteScroll
+    on:thresholdReached={() => {
+      fetchMore()
+    }}
+  />
+{/if}
+
+{#if mode === 'paginated'}
+  <div class="bottom-space">
+    <Pagination
+      disabledNext={!nextCursor || isLoadingCursor === nextCursor}
+      disabledPrevious={!previousCursor || isLoadingCursor === previousCursor}
+      on:next={nextPage}
+      on:previous={previousPage}
+    />
+  </div>
+{/if}
+
+<style lang="scss">
+  .bottom-space {
+    margin-bottom: var(--space-lg);
+  }
+</style>
