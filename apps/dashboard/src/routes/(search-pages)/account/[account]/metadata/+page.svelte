82db1f12a91117fe6d0e294a257dd27b2ab2def9
Dawid Sowa <dawid.sowa@rdx.works>
2023-07-13 12:37:22 +0200

    feat: add defaults display based on metadata type

--- apps/dashboard/src/routes/(search-pages)/account/[account]/metadata/+page.svelte
+++ apps/dashboard/src/routes/(search-pages)/account/[account]/metadata/+page.svelte
@@ -1,56 +1,18 @@
 <script lang="ts">
-  import Tags from '@components/_base/tags/Tags.svelte'
-  import Link from '@components/_base/link/Link.svelte'
-  import AddressesList from '@components/_base/address/AddressesList.svelte'
-  import MetadataInfoBox from '@components/metadata-info-box/MetadataInfoBox.svelte'
-  import type { EntityMetadataItemValue } from '@radixdlt/babylon-gateway-api-sdk'
-  import IconNew from '@components/_base/icon/IconNew.svelte'
+  import MetadataInfoBox, {
+    type ExtendedEntityMetadataItem
+  } from '@components/metadata-info-box/MetadataInfoBox.svelte'
   import type { LayoutData } from '../$types'
 
   export let data: LayoutData
 
-  const expectedEntries = {
-    account_type: {},
-    name: {},
-    description: {},
-    tags: {
-      component: Tags,
-      componentProperties: (value: EntityMetadataItemValue) => ({
-        showNetworkTag: false,
-        tags: value.as_string_collection
-      })
-    },
-    icon_url: {
-      label: 'Icon',
-      component: IconNew,
-      componentProperties: (value: EntityMetadataItemValue) => ({
-        icon: value.as_string,
-        size: 'large',
-        rounded: true
-      })
-    },
-    claimed_entities: {
-      component: AddressesList,
-      componentProperties: (value: EntityMetadataItemValue) => ({
-        addresses: value.as_string_collection
-      })
-    },
-    claimed_website: {
-      component: Link,
-      componentProperties: (value: EntityMetadataItemValue) => ({
-        url: value.as_string,
-        external: true
-      })
-    }
-  }
-
   const metadata = data.promises.entityDetails.then(
     ({ metadata }) => metadata.items
-  )
+  ) as unknown as Promise<ExtendedEntityMetadataItem[]>
 </script>
 
 <div class="surface-2">
-  <MetadataInfoBox {metadata} {expectedEntries} />
+  <MetadataInfoBox {metadata} />
 </div>
 
 <style lang="scss">
