76ac82496ac0e498a0515beac9ec905f367c632a
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-07-14 13:08:15 +0100

    PTD-753 fix: checkbox click behavior

--- packages/ui/src/components/_base/checkbox/Checkbox.svelte
+++ packages/ui/src/components/_base/checkbox/Checkbox.svelte
@@ -14,14 +14,14 @@
   }
 </script>
 
-<div class="wrapper">
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click|stopPropagation={handleOnClick} class="wrapper">
   <label class="label">
     <input
       class="checkbox"
       type="checkbox"
       class:checked
       bind:checked
-      on:click|stopPropagation={handleOnClick}
       style={checked
         ? `background: center / contain no-repeat url(${CheckedIcon});`
         : ''}
@@ -38,17 +38,19 @@
     display: flex;
     flex-direction: column;
     gap: var(--spacing-sm);
+    width: fit-content;
   }
   .label {
+    cursor: pointer;
     display: flex;
     font-weight: var(--font-weight-bold-2);
     color: var(--label-color);
   }
   .checkbox {
-    appearance: none;
     cursor: pointer;
     width: $box-size;
     height: $box-size;
+    appearance: none;
     border: $border-width solid var(--color-grey-2);
     border-radius: var(--border-radius-sm);
     margin: 0 var(--spacing-sm) 0 0;
