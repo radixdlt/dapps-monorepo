358b7ba234e1f13fd2f34d2c5f490d0092c7c8ab
Alex Stelea <alex.s@rdx.works>
2023-07-26 09:11:02 +0200

    style: remove arrows in number input

--- packages/ui/src/components/_base/input/Input.svelte
+++ packages/ui/src/components/_base/input/Input.svelte
@@ -37,3 +37,11 @@
   class={style}
   {placeholder}
 />
+
+<style lang="scss">
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+</style>
