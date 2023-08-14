942faaa84733540a685fb75c51ca25c130a5dc0b
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-08-14 15:16:41 +0100

    add log

--- apps/dashboard/svelte.config.js
+++ apps/dashboard/svelte.config.js
@@ -2,6 +2,8 @@ import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-node';
 import { aliases } from '../../aliases.js';
 
+console.log('ENV DIR LOCATION', `${process.cwd()}/../../`)
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
