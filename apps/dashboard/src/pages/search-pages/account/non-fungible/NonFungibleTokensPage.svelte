07349504ecd74917ef0eb55ea3f68235daab899c
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-08-22 16:13:22 +0100

    feat: add non fungible tokens page

---
+++ apps/dashboard/src/pages/search-pages/account/non-fungible/NonFungibleTokensPage.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import type { NonFungible, NonFungibleResource } from '@api/utils/resources'
+  import NFTAccordion from '@components/_base/accordion/NFTAccordion.svelte'
+  import NonFungibleTokenCard from '@components/non-fungible-token-card/NonFungibleTokenCard.svelte'
+
+  export let nonFungibleResources: Promise<
+    {
+      resource: NonFungibleResource
+      totalNonFungibles: number
+      nonFungibles: NonFungible[]
+    }[]
+  >
+</script>
+
+{#await nonFungibleResources}
+  loading...
+{:then nonFungibleResources}
+  {#each nonFungibleResources as { resource, nonFungibles, totalNonFungibles }}
+    <NFTAccordion
+      name={resource.name}
+      address={resource.address}
+      imageUrl={resource.iconUrl}
+      count={nonFungibles.length}
+      tags={resource.tags}
+      totalCount={totalNonFungibles}
+    >
+      <div class="nft-cards">
+        {#each nonFungibles as nft}
+          <NonFungibleTokenCard
+            imgUrl={nft.iconUrl}
+            name={nft.name}
+            id={nft.id}
+          />
+        {/each}
+      </div>
+    </NFTAccordion>
+  {/each}
+{/await}
+
+<style>
+  .nft-cards {
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--spacing-2xl);
+  }
+</style>
