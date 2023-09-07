9483c3ea2e8b318c0131e6526e804e5ca8037e96
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-09-07 11:47:49 +0100

    feat: add pool units view

---
+++ apps/dashboard/src/pages/search-pages/account/pool-units/PoolUnitCards.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte'
+  import PoolUnitCard from '@components/pool-unit-card/PoolUnitCard.svelte'
+  import type PoolUnits from './PoolUnits.svelte'
+
+  export let poolUnits: ComponentProps<PoolUnits>['poolUnits']
+</script>
+
+<div class="pool-units">
+  {#await poolUnits then poolUnits}
+    {#each poolUnits as poolUnit}
+      <div class="pool-unit">
+        <PoolUnitCard {poolUnit} />
+      </div>
+    {/each}
+  {/await}
+</div>
+
+<style>
+  .pool-units {
+    flex-grow: 1;
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--spacing-2xl);
+    align-content: flex-start;
+    flex-direction: column;
+    justify-content: stretch;
+  }
+
+  .pool-unit {
+    width: 100%;
+  }
+</style>
---
+++ apps/dashboard/src/routes/(search-pages)/account/[account]/pool-units/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import PoolUnits from '@dashboard-pages/search-pages/account/pool-units/PoolUnits.svelte'
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+<PoolUnits
+  stakeInfo={data.promises.stakeInfo}
+  poolUnits={data.promises.poolUnits}
+/>
