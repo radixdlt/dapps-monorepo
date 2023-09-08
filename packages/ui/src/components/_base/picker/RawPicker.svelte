92fb83ed9087ee90b91039e9743af13cae421df9
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-09-08 15:25:14 +0100

    refactor: pickers

---
+++ packages/ui/src/components/_base/picker/RawPicker.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+  import { createEventDispatcher, onMount } from 'svelte'
+  import { fly } from 'svelte/transition'
+
+  type T = $$Generic
+
+  type Options<V> = Readonly<
+    {
+      label: string
+      value: V
+    }[]
+  >
+
+  type $$Events = {
+    selected: {
+      detail: Options<T>[number]
+    }
+  }
+
+  export let options: Options<T>
+  export let open = false
+
+  const dispatch = createEventDispatcher<{
+    selected: (typeof options)[number]
+  }>()
+
+  $: selected = options[0]
+
+  onMount(() => {
+    dispatch('selected', selected)
+  })
+
+  let picker: HTMLElement
+
+  onMount(() => {
+    picker.addEventListener('clickoutside', () => (open = false))
+
+    const handleClick = (event: any) => {
+      if (!picker.contains(event.target)) {
+        picker.dispatchEvent(new CustomEvent('clickoutside'))
+      }
+    }
+
+    document.addEventListener('click', handleClick, true)
+
+    return () => document.removeEventListener('click', handleClick, true)
+  })
+
+  let offset: number
+  let width: number
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="picker" bind:clientWidth={width} bind:this={picker}>
+  <div
+    class="option selected"
+    on:click={() => (open = !open)}
+    bind:clientHeight={offset}
+  >
+    <slot name="selected" {selected} />
+  </div>
+
+  {#if open}
+    <div
+      class="drawer"
+      style:transform={`translateY(${offset}px)`}
+      style:max-height={`${offset * 7}px`}
+      in:fly={{ y: -10, duration: 150 }}
+    >
+      <slot name="options-header" />
+      {#each options as option}
+        <div
+          class="option"
+          on:click={() => {
+            dispatch('selected', option)
+            selected = option
+            open = false
+          }}
+        >
+          <slot name="option" {option} />
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .picker {
+    display: flex;
+    flex-direction: column;
+    z-index: 1;
+  }
+
+  .drawer {
+    position: absolute;
+    width: 100%;
+    overflow-y: auto;
+  }
+
+  .option {
+    cursor: pointer;
+
+    &.selected {
+      z-index: 1;
+    }
+  }
+</style>
--- packages/ui/src/components/_base/picker/account-picker/AccountPicker.svelte
+++ packages/ui/src/components/_base/picker/account-picker/AccountPicker.svelte
@@ -1,78 +1,41 @@
 <script lang="ts">
+  import Account from '@components/_base/account/Account.svelte'
+  import Picker from '../Picker.svelte'
+  import AccountPickerExpandIcon from '@icons/account-picker-expand.svg'
   import { accounts, type Account as AccountType } from '@stores'
+  import IconNew from '@components/_base/icon/IconNew.svelte'
   import Radio from '@components/_base/radio/Radio.svelte'
 
-  import IconNew from '@components/_base/icon/IconNew.svelte'
-  import Picker from '../Picker.svelte'
-  import Account from '@components/_base/account/Account.svelte'
-  import AccountPickerExpandIcon from '@icons/account-picker-expand.svg'
-
-  export let selected: AccountType | undefined = undefined
+  export let selected: AccountType
 
   $: options = $accounts.map((account) => ({
     label: account.label,
     value: account
   }))
-
-  $: selected = options[0]?.value
-
-  let open = false
 </script>
 
-<div class="account-picker">
-  <Picker
-    {options}
-    on:selected={({ detail }) => (selected = detail.value)}
-    bind:open
-  >
-    <div slot="selected" class="selected-wrapper">
-      <div class="selected" class:open>
-        <Account account={selected}>
-          <div class="icon" style:transform={`rotate(${open ? '-180deg' : 0})`}>
-            <IconNew icon={AccountPickerExpandIcon} size="small" />
-          </div>
-        </Account>
+<Picker
+  {options}
+  selectionText="Select Account"
+  on:selected={(e) => (selected = e.detail.value)}
+>
+  <svelte:fragment slot="selected" let:open let:selected>
+    <Account account={selected.value}>
+      <div class="icon" style:transform={`rotate(${open ? '-180deg' : 0})`}>
+        <IconNew icon={AccountPickerExpandIcon} size="small" />
       </div>
-    </div>
+    </Account>
+  </svelte:fragment>
 
-    <div class="options-header" slot="options-header">Select account</div>
+  <svelte:fragment slot="option" let:option>
+    <Account account={option.value}>
+      <Radio selected={option.value === selected} />
+    </Account>
+  </svelte:fragment>
+</Picker>
 
-    <div slot="option" let:option class="option-wrapper">
-      <Account account={option.value}>
-        <Radio selected={option.value === selected} />
-      </Account>
-    </div>
-  </Picker>
-</div>
-
-<style lang="scss">
-  .account-picker :global(.picker .drawer) {
-    background: var(--theme-surface-2);
-    border-radius: var(--border-radius-xl);
-    padding: var(--spacing-lg);
-    box-shadow: var(--shadow-hover);
-    margin-top: var(--spacing-lg);
-  }
-
-  .selected-wrapper {
-    overflow-y: clip;
-  }
-
+<style>
   .icon {
     transition: transform 0.3s ease;
-  }
-
-  .options-header {
-    font-weight: var(--font-weight-bold-3);
-    font-size: var(--text-md);
-    color: var(--theme-subtext);
-  }
-
-  .option-wrapper {
-    margin-top: var(--spacing-lg);
-    transition: opacity 300ms ease;
-    &:hover {
-      opacity: 50%;
-    }
   }
 </style>
