d84dddec64e5e2b4067612642d766742fabc50ed
Alex Stelea <alex.s@rdx.works>
2023-09-11 14:08:39 +0200

    chore: update csp directives

--- apps/dashboard/svelte.config.js
+++ apps/dashboard/svelte.config.js
@@ -16,7 +16,34 @@ const config = {
     csp: {
       mode: 'auto',
       directives: {
-        'default-src': ['self']
+        'default-src': ['self'],
+        'style-src-elem': [
+          'self',
+          'unsafe-inline',
+          'https://fonts.googleapis.com/css2',
+          'https://fonts.cdnfonts.com/css/hack'
+        ],
+        'font-src': [
+          'self',
+          'https://fonts.gstatic.com',
+          'https://fonts.cdnfonts.com/s/16184/Hack-Regular.woff',
+          'https://fonts.cdnfonts.com/s/16184/Hack-Italic.woff',
+          'https://fonts.cdnfonts.com/s/16184/Hack-Bold.woff',
+          'https://fonts.cdnfonts.com/s/16184/Hack-BoldItalic.woff',
+          'data:'
+        ],
+        'img-src': ['self', 'data:', 'https://cdn-ukwest.onetrust.com'],
+        'connect-src': [
+          'self',
+          'https://*.radixdlt.com',
+          'https://cdn-ukwest.onetrust.com',
+          'https://geolocation.onetrust.com/cookieconsentpub/v1/geo/location'
+        ],
+        'script-src': [
+          'self',
+          'https://cdn-ukwest.onetrust.com',
+          'sha256-sk0iIQli9YGfhwrJFHurkbv5hqBs+BsyGKzjsaUAokM='
+        ]
       }
     }
   }
