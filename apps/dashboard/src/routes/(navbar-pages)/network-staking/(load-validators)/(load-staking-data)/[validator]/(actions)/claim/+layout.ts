cfba34087c44527bcaea79d1abadb7855f32d49c
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-09-21 11:07:47 +0100

    fix: claim all

--- apps/dashboard/src/routes/(navbar-pages)/network-staking/(load-validators)/(load-staking-data)/[validator]/(actions)/claim/+layout.ts
+++ apps/dashboard/src/routes/(navbar-pages)/network-staking/(load-validators)/(load-staking-data)/[validator]/(actions)/claim/+layout.ts
@@ -1,21 +1,10 @@
 import { derived } from 'svelte/store'
 import type { LayoutLoad } from './$types'
-import BigNumber from 'bignumber.js'
 
 export const load: LayoutLoad = ({ parent }) =>
   parent().then(async (data) => {
-    const stakes = await data.stakes
-
-    const readyToClaim = derived(stakes, ($stakes) =>
-      $stakes.then((stakes) =>
-        stakes
-          .map(({ account, validator, readyToClaim }) => ({
-            account,
-            validator,
-            xrdAmount: readyToClaim
-          }))
-          .filter(({ xrdAmount }) => !new BigNumber(xrdAmount).eq(0))
-      )
+    const readyToClaim = derived(data.stakeInfo, ($info) =>
+      $info.then(({ readyToClaim }) => readyToClaim)
     )
 
     return {
