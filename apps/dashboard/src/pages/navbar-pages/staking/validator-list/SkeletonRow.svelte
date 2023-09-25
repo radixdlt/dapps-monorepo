df0891875bb2a012a7b9b6b7ea2343bf10ffc5ac
AbstractFruitFactory <alexmedkex@gmail.com>
2023-09-25 11:39:35 +0100

    feat: validator list loading state (#697)

---
+++ apps/dashboard/src/pages/navbar-pages/staking/validator-list/SkeletonRow.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { SkeletonLoader } from '@aleworm/svelte-skeleton-loader'
+  import ResponsiveTableCell from '@components/_base/table/basic-table/ResponsiveTableCell.svelte'
+  import TableRow from '@components/_base/table/basic-table/TableRow.svelte'
+
+  export let columns: number
+</script>
+
+<TableRow>
+  <ResponsiveTableCell colspan={columns}>
+    <SkeletonLoader />
+  </ResponsiveTableCell>
+</TableRow>
