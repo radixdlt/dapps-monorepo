21f72974df10a1ae02958a2e855bab8f2b4df9be
AbstractFruitFactory <alexmedkex@gmail.com>
2023-09-25 15:15:11 +0100

    feat: add footer (#706)

---
+++ packages/ui/src/components/footer/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Divider from '@components/_base/divider/Divider.svelte'
+</script>
+
+<div>
+  <Divider />
+
+  <div class="footer">
+    <a href="https://www.radixdlt.com/privacy-policy" target="_blank"
+      >Privacy Notice</a
+    >
+  </div>
+</div>
+
+<style>
+  .footer {
+    display: flex;
+    justify-content: center;
+    margin-bottom: var(--spacing-2xl);
+  }
+</style>
