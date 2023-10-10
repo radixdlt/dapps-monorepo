fa7e78f76344b6cf82693eb0fb542839f799dd68
Dawid Sowa <dawid.sowa@rdx.works>
2023-10-10 14:20:53 +0200

    ci: remove unnecessary build command

--- apps/dashboard/src/server/db/context.ts
+++ apps/dashboard/src/server/db/context.ts
@@ -1,7 +1,7 @@
 import type { PrismaClient } from '@prisma/client'
 import { mockDeep, type DeepMockProxy } from 'vitest-mock-extended'
 import type { Cookies } from '@sveltejs/kit'
-import type { GatewayApiClient } from '@common/gateway-sdk'
+import type { GatewayApiClient } from '@common/rdt'
 
 export type Context = {
   prisma: PrismaClient
