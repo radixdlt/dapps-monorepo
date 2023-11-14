24199b9803d0d16c23d3c68f7d7e2d57c22b7abf
Alex Stelea <alex.s@rdx.works>
2023-11-14 10:40:50 +0100

    style: use variable

--- packages/ui/src/dev-console/form/SelectItems.svelte
+++ packages/ui/src/dev-console/form/SelectItems.svelte
@@ -6,6 +6,6 @@
   div {
     border-radius: 8px;
     background: var(--theme-surface-2);
-    box-shadow: 0px 6.72099px 11px 0px rgba(0, 0, 0, 0.06);
+    box-shadow: var(--shadow-hover);
   }
 </style>
