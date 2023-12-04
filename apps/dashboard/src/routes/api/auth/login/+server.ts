341e1d39d7155bdfa9c7378ec7194cb2465e97ab
Alex Stelea <alex.s@rdx.works>
2023-12-04 16:57:59 +0100

    chore: add error logging

--- apps/dashboard/src/routes/api/auth/login/+server.ts
+++ apps/dashboard/src/routes/api/auth/login/+server.ts
@@ -1,14 +1,17 @@
 import type { RequestHandler } from './$types'
 import { error, json } from '@sveltejs/kit'
 import { authController } from '../../../../server/auth/controller'
+import { appLogger } from '@dashboard/server/helpers/logger'
 
 /** @type {import('./$types').RequestHandler} */
 export const POST: RequestHandler = async ({ request, cookies }) => {
   const requestBody = await request.json()
   const result = await authController.login(requestBody, cookies)
 
-  if (result.isErr())
+  if (result.isErr()) {
+    appLogger.error(result.error)
     throw error(result.error.httpResponseCode, result.error.reason)
+  }
 
   const { authToken, headers } = result.value.data
 
