af65ff5355e7db476fd11fcbaf6bf52a0141791b
Dawid Sowa <119939994+dawidsowardx@users.noreply.github.com>
2024-01-04 12:36:31 +0100

    Bump dependencies (#883)
    * feat: bump RDT, add stokenet to dapps-dropdown
    
    * fix prettier

--- packages/ui/src/components/dapps-dropdown/DappsDropdown.svelte
+++ packages/ui/src/components/dapps-dropdown/DappsDropdown.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+  import { RadixNetwork } from '@common/gateway-sdk'
+  import { CURRENT_NETWORK } from '@networks'
   import '@radixdlt/dapps-dropdown'
+
+  const networkName =
+    CURRENT_NETWORK.id === RadixNetwork.Mainnet ? 'Mainnet' : 'Stokenet'
 </script>
 
-<radix-dapps-dropdown />
+<radix-dapps-dropdown {networkName} />
