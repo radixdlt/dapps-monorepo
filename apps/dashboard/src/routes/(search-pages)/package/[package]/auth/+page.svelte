53f593eab5b9937209f7fc5e3dcdd538f108cf8e
Dawid Sowa <dawid.sowa@rdx.works>
2024-05-21 10:59:58 +0200

    feat(dashboard): add `configuration` tab to package entity

---
+++ apps/dashboard/src/routes/(search-pages)/package/[package]/auth/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import AuthConfigurationTab from '$lib/auth/AuthConfigurationTab.svelte'
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+{#await Promise.all([data.promises.package]) then [{ auth }]}
+  <AuthConfigurationTab {auth} />
+{/await}
