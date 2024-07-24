311438a78ce08b5f2871d6960d36d83596997b69
Dawid Sowa <dawid.sowa@rdx.works>
2024-07-25 01:34:56 +0200

    fix(ui): hide header responsiveness

--- packages/ui/src/directives/hide-header.ts
+++ packages/ui/src/directives/hide-header.ts
@@ -1,30 +1,24 @@
 export function hideHeader(node: HTMLElement) {
-  let throttlePause: boolean = false
   let lastScrollTop = 0
-  const throttle = (callback: () => void, time: number) => {
-    if (throttlePause) return
-    //set throttlePause to true after the if condition. This allows the function to be run once
-    throttlePause = true
 
-    //setTimeout runs the callback within the specified time
-    setTimeout(() => {
-      callback()
-
-      //throttlePause is set to false once the function has been called, allowing the throttle function to loop
-      throttlePause = false
-    }, time)
-  }
   const handleScroll = (event: Event) => {
-    throttle(() => {
-      const { scrollTop } = event.target as Element
-      if (scrollTop > lastScrollTop) {
-        node.classList.add('collapsed')
-        lastScrollTop = scrollTop
-      } else if (scrollTop === 0 || lastScrollTop - 400 > scrollTop) {
-        lastScrollTop = scrollTop
-        node.classList.remove('collapsed')
+    const { scrollTop } = event.target as Element
+    if (scrollTop === 0) {
+      lastScrollTop = 0
+      node.classList.remove('collapsed')
+      return
+    }
+
+    if (scrollTop > lastScrollTop) {
+      if (scrollTop === 0) {
+        return
       }
-    }, 300)
+      node.classList.add('collapsed')
+      lastScrollTop = scrollTop
+    } else if (scrollTop === 0 || lastScrollTop - 400 > scrollTop) {
+      lastScrollTop = scrollTop
+      node.classList.remove('collapsed')
+    }
   }
 
   node.classList.remove('collapsed')
