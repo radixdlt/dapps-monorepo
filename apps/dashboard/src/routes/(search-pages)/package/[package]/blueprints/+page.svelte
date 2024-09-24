0787766bd226ede1dc6ef7b2c77ea53567e6791a
Dawid Sowa <dawid.sowa@rdx.works>
2024-09-24 17:37:44 +0200

    feat(dashboard): add blueprints tab to package entity

---
+++ apps/dashboard/src/routes/(search-pages)/package/[package]/blueprints/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Row from '@components/info-box/Row.svelte'
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+{#await data.promises.package then _package}
+  {#each _package.blueprints as blueprint}
+    <div class="card">
+      <h3>{blueprint.name}</h3>
+      {#each blueprint.methods as method}
+        <Row modifiers="no-capitalize" text={method.name} />
+      {/each}
+    </div>
+  {/each}
+{/await}
+
+<style lang="scss">
+  .card:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+</style>
