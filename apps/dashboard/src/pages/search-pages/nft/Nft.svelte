6d8fe8227fcb9ee24ac304d518418283662aa6f7
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-10-15 23:05:47 +0100

    feat: when nft doesnt exist still show collection details

--- apps/dashboard/src/pages/search-pages/nft/Nft.svelte
+++ apps/dashboard/src/pages/search-pages/nft/Nft.svelte
@@ -5,6 +5,7 @@
   import NftDataRow from './NftDataRow.svelte'
   import type { NonFungible } from '@api/utils/nfts'
   import type { NonFungibleResource } from '@api/utils/entities/resource/non-fungible'
+  import { networkConfiguration } from '@stores'
 
   export let nft: Promise<NonFungible | undefined>
   export let resource: Promise<NonFungibleResource>
@@ -28,57 +29,78 @@
       : resource.metadata.expected.description?.typed.value
   )
 
+  let hasNoNft: boolean
+  $: Promise.all([nft, resource]).then(([nft, resource]) => {
+    const resourceAddresses = $networkConfiguration?.well_known_addresses
+    if (
+      ![
+        resourceAddresses?.ed25519_signature_virtual_badge,
+        resourceAddresses?.package_of_direct_caller_virtual_badge,
+        resourceAddresses?.secp256k1_signature_virtual_badge,
+        resourceAddresses?.global_caller_virtual_badge
+      ].includes(resource.address)
+    ) {
+      hasNoNft = !nft
+    }
+  })
+
   const imageSize = 'large'
 </script>
 
-<div class="nft-image">
-  {#await imageUrl}
-    <NftImage size={imageSize} />
-  {:then url}
-    <NftImage {url} size={imageSize} />
-  {/await}
-</div>
-
-<div class="card info-card">
-  <h2>
-    {#await nft}
-      <SkeletonLoader />
-    {:then nft}
-      {#if nft}
-        {@const {
-          nftData: {
-            expected: { name }
-          }
-        } = nft}
-        {#if name?.value}
-          {name?.value}
-        {/if}
-      {/if}
-    {/await}
-  </h2>
-
-  {#await description}
-    <SkeletonLoader count={3} />
-  {:then description}
-    {#if description}
-      {description}
-    {/if}
-  {/await}
-  <div>
-    {#await nft}
-      <SkeletonLoader />
-    {:then nft}
-      {#if nft}
-        <NftDataRow
-          value={{ kind: 'String', field_name: 'ID', value: nft.id }}
-        />
-        {#each nft.type === 'generalNft' ? nft.nftData.nonStandard : Object.values(nft.nftData.expected).filter((data) => data.field_name !== 'name') as value}
-          <NftDataRow {value} />
-        {/each}
-      {/if}
+{#if hasNoNft}
+  <div class="no-nft">
+    <h2>There is no non-fungible with this local ID</h2>
+  </div>
+{:else}
+  <div class="nft-image">
+    {#await imageUrl}
+      <NftImage size={imageSize} />
+    {:then url}
+      <NftImage {url} size={imageSize} />
     {/await}
   </div>
-</div>
+
+  <div class="card info-card">
+    <h2>
+      {#await nft}
+        <SkeletonLoader />
+      {:then nft}
+        {#if nft}
+          {@const {
+            nftData: {
+              expected: { name }
+            }
+          } = nft}
+          {#if name?.value}
+            {name?.value}
+          {/if}
+        {/if}
+      {/await}
+    </h2>
+
+    {#await description}
+      <SkeletonLoader count={3} />
+    {:then description}
+      {#if description}
+        {description}
+      {/if}
+    {/await}
+    <div>
+      {#await nft}
+        <SkeletonLoader />
+      {:then nft}
+        {#if nft}
+          <NftDataRow
+            value={{ kind: 'String', field_name: 'ID', value: nft.id }}
+          />
+          {#each nft.type === 'generalNft' ? nft.nftData.nonStandard : Object.values(nft.nftData.expected).filter((data) => data.field_name !== 'name') as value}
+            <NftDataRow {value} />
+          {/each}
+        {/if}
+      {/await}
+    </div>
+  </div>
+{/if}
 
 <h2 class="resource-card-header">Belongs To:</h2>
 
@@ -90,6 +112,14 @@
 />
 
 <style lang="scss">
+  .no-nft {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    margin: var(--spacing-4xl) 0;
+  }
+
   .nft-image {
     width: fit-content;
     margin: var(--spacing-2xl) auto var(--spacing-3xl) auto;
